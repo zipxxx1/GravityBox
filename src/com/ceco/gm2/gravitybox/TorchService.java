775a47dc0717b9173c800943045e5440532e0a64
C3C0 <pete.gregus@gmail.com>
2013-12-06 12:55:58 +0100

    TorchService improvements - implemented partial wakelock which allows torch to be on when screen is off on some devices - better handling of start command - finish explicitly when torch set to off

--- src/com/ceco/gm2/gravitybox/TorchService.java
+++ src/com/ceco/gm2/gravitybox/TorchService.java
@@ -18,14 +18,20 @@ package com.ceco.gm2.gravitybox;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+
+import com.ceco.gm2.gravitybox.R;
 
 public class TorchService extends Service {
+    private static final String TAG = "GB:TorchService";
 
     public static final String ACTION_TOGGLE_TORCH = "gravitybox.intent.action.TOGGLE_TORCH";
     public static final String ACTION_TORCH_STATUS_CHANGED = "gravitybox.intent.action.TORCH_STATUS_CHANGED";
@@ -38,6 +44,7 @@ public class TorchService extends Service {
     private int mTorchStatus = TORCH_STATUS_OFF;
     private Notification mTorchNotif;
     private PendingIntent mPendingIntent;
+    private WakeLock mPartialWakeLock;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -64,14 +71,14 @@ public class TorchService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent != null && intent.getAction() != null &&
-                intent.getAction().equals(ACTION_TOGGLE_TORCH)) {
+        if (intent != null && ACTION_TOGGLE_TORCH.equals(intent.getAction())) {
             toggleTorch();
-            return START_STICKY;
+            return START_REDELIVER_INTENT;
+        } else {
+            stopSelf();
+            return START_NOT_STICKY;
         }
-
-        stopSelf();
-        return START_NOT_STICKY;
+        
     }
 
     private synchronized void toggleTorch() {
@@ -92,9 +99,12 @@ public class TorchService extends Service {
             mCamera.setParameters(camParams);
             mCamera.setPreviewTexture(new SurfaceTexture(0));
             mCamera.startPreview();
-
             mTorchStatus = TORCH_STATUS_ON;
             startForeground(2, mTorchNotif);
+
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE); 
+            mPartialWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+            mPartialWakeLock.acquire(3600000);
         } catch (Exception e) {
             mTorchStatus = TORCH_STATUS_ERROR;
             e.printStackTrace();
@@ -102,19 +112,26 @@ public class TorchService extends Service {
             Intent i = new Intent(ACTION_TORCH_STATUS_CHANGED);
             i.putExtra(EXTRA_TORCH_STATUS, mTorchStatus);
             sendBroadcast(i);
+            if (mTorchStatus == TORCH_STATUS_ERROR) {
+                stopSelf();
+            }
         }
     }
 
     private synchronized void setTorchOff() {
-        if (mCamera == null) return;
-
         try {
-            Camera.Parameters camParams = mCamera.getParameters();
-            camParams.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-            mCamera.setParameters(camParams);
-            mCamera.stopPreview();
-            mCamera.release();
-            mCamera = null;
+            if (mPartialWakeLock != null && mPartialWakeLock.isHeld()) {
+                mPartialWakeLock.release();
+                mPartialWakeLock = null;
+            }
+            if (mCamera != null) {
+                Camera.Parameters camParams = mCamera.getParameters();
+                camParams.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                mCamera.setParameters(camParams);
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            }
             mTorchStatus = TORCH_STATUS_OFF;
         } catch (Exception e) {
             mTorchStatus = TORCH_STATUS_ERROR;
@@ -124,6 +141,7 @@ public class TorchService extends Service {
             i.putExtra(EXTRA_TORCH_STATUS, mTorchStatus);
             sendBroadcast(i);
             stopForeground(true);
+            stopSelf();
         }
     }
 
