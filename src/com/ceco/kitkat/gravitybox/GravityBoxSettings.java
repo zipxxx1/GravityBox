9e698d8eda0ff1cbd39d62dfe57e4ba23bd2fdb6
maqr <maqr.lollerskates@gmail.com>
2014-01-02 09:05:25 +0100

    Added Auto Rotation Toggle as a hardware key option.
    (cherry picked from commit 35140055518cca505b79d2b1c27c8feb957c1d30)
    
    Revised by C3C076:
    - Toasts cannot be used directly within PhoneWindowManager instance
    since its thread has no Looper. Use its handler for executing action.

--- src/com/ceco/kitkat/gravitybox/GravityBoxSettings.java
+++ src/com/ceco/kitkat/gravitybox/GravityBoxSettings.java
@@ -269,6 +269,7 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
     public static final int HWKEY_ACTION_HOME = 13;
     public static final int HWKEY_ACTION_BACK = 14;
     public static final int HWKEY_ACTION_SCREEN_RECORDING = 15;
+    public static final int HWKEY_ACTION_AUTO_ROTATION = 16;
     public static final int HWKEY_DOUBLETAP_SPEED_DEFAULT = 400;
     public static final int HWKEY_KILL_DELAY_DEFAULT = 1000;
     public static final int HWKEY_TORCH_DISABLED = 0;
