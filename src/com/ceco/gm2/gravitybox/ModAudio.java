5bdd6dd9ae45a04d096f2c178f1a14ddff1e1959
C3C0 <ceco@ceco.sk.eu.org>
2014-01-10 19:11:30 +0100

    ModAudio: option to force music volume control When enabled, volume keys control music volume instead of ringer volume
    Cherry-picked and backported from KitKat branch
    
    Revised by bgcngm:
    - adapted to take into consideration the following:
    -- checkUpdateRemoteStateIfActive method resides on android.media.AudioService
    -- isAfMusicActiveRecently method does not exist, but workaround was arranged
    (http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/4.4_r1/android/media/AudioService.java#2641)

--- src/com/ceco/gm2/gravitybox/ModAudio.java
+++ src/com/ceco/gm2/gravitybox/ModAudio.java
@@ -20,11 +20,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.XResources;
+import android.media.AudioManager;
 import android.os.Build;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 
 public class ModAudio {
     private static final String TAG = "GB:ModAudio";
@@ -32,11 +34,14 @@ public class ModAudio {
     private static final String CLASS_VOLUME_STREAM_STATE = "android.media.AudioService$VolumeStreamState";
     private static final String CLASS_AUDIO_SYSTEM = "android.media.AudioSystem";
     private static final String CLASS_AUDIO_SERVICE = "android.media.AudioService";
-    private static final int STREAM_MUSIC = 3;
-    private static final int VOLUME_STEPS = 30;
     private static final boolean DEBUG = false;
 
+    private static final int STREAM_MUSIC = 3;
+    private static final int VOLUME_STEPS = 30;
+    private static final int DEFAULT_STREAM_TYPE_OVERRIDE_DELAY_MS = 5000;
+
     private static boolean mSafeMediaVolumeEnabled;
+    private static boolean mVolForceMusicControl;
 
     private static void log(String message) {
         XposedBridge.log(TAG + ": " + message);
@@ -51,6 +56,10 @@ public class ModAudio {
                 mSafeMediaVolumeEnabled = intent.getBooleanExtra(
                         GravityBoxSettings.EXTRA_SAFE_MEDIA_VOLUME_ENABLED, false);
                 if (DEBUG) log("Safe headset media volume set to: " + mSafeMediaVolumeEnabled);
+            } else if (intent.getAction().equals(GravityBoxSettings.ACTION_PREF_VOL_FORCE_MUSIC_CONTROL_CHANGED)) {
+                mVolForceMusicControl = intent.getBooleanExtra(
+                        GravityBoxSettings.EXTRA_VOL_FORCE_MUSIC_CONTROL, false);
+                if (DEBUG) log("Force music volume control set to: " + mVolForceMusicControl);
             }
         }
     };
@@ -58,6 +67,7 @@ public class ModAudio {
     public static void initZygote(final XSharedPreferences prefs) {
         try {
             final Class<?> classAudioService = XposedHelpers.findClass(CLASS_AUDIO_SERVICE, null);
+            final Class<?> classAudioSystem = XposedHelpers.findClass(CLASS_AUDIO_SYSTEM, null);
 
             if (prefs.getBoolean(GravityBoxSettings.PREF_KEY_MUSIC_VOLUME_STEPS, false)
                     && Utils.shouldAllowMoreVolumeSteps()) {
@@ -73,6 +83,7 @@ public class ModAudio {
     
                     IntentFilter intentFilter = new IntentFilter();
                     intentFilter.addAction(GravityBoxSettings.ACTION_PREF_SAFE_MEDIA_VOLUME_CHANGED);
+                    intentFilter.addAction(GravityBoxSettings.ACTION_PREF_VOL_FORCE_MUSIC_CONTROL_CHANGED);
                     context.registerReceiver(mBroadcastReceiver, intentFilter);
                     if (DEBUG) log("AudioService constructed. Broadcast receiver registered");
                 }
@@ -105,6 +116,38 @@ public class ModAudio {
                     }
                 });
             }
+
+            mVolForceMusicControl = prefs.getBoolean(
+                    GravityBoxSettings.PREF_KEY_VOL_FORCE_MUSIC_CONTROL, false);
+            XposedHelpers.findAndHookMethod(classAudioService, "getActiveStreamType",
+                    int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    if (mVolForceMusicControl &&
+                            (Integer) param.args[0] == AudioManager.USE_DEFAULT_STREAM_TYPE) {
+                        final boolean voiceCapable = XposedHelpers.getBooleanField(
+                                param.thisObject, "mVoiceCapable");
+                        final boolean isInComm = (Boolean) XposedHelpers.callMethod(
+                                param.thisObject, "isInCommunication");
+                        boolean activeMusic = (Boolean) XposedHelpers.callStaticMethod(
+                                classAudioSystem, "isStreamActive",
+                                STREAM_MUSIC, DEFAULT_STREAM_TYPE_OVERRIDE_DELAY_MS);
+                        if (Build.VERSION.SDK_INT > 17) {
+                            activeMusic = activeMusic || (Boolean) XposedHelpers.callStaticMethod(
+                                    classAudioSystem, "isStreamActiveRemotely",
+                                    STREAM_MUSIC, DEFAULT_STREAM_TYPE_OVERRIDE_DELAY_MS);
+                        }
+                        final boolean isRemoteMusic = (Boolean) XposedHelpers.callMethod(
+                                param.thisObject, "checkUpdateRemoteStateIfActive",
+                                STREAM_MUSIC);
+
+                        if (voiceCapable && !isInComm && !activeMusic && !isRemoteMusic) {
+                            param.setResult(STREAM_MUSIC);
+                            if (DEBUG) log("getActiveStreamType: Forcing music stream");
+                        }
+                    }
+                }
+            });
         } catch(Throwable t) {
             XposedBridge.log(t);
         }
