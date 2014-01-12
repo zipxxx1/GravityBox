7722d67f0e4233bcc36159116f92b677964162ca
C3C0 <ceco@ceco.sk.eu.org>
2014-01-12 10:54:19 +0000

    Misc: Option to define Torch Auto off Serves mainly for LED protection purposes

--- src/com/ceco/gm2/gravitybox/GravityBoxSettings.java
+++ src/com/ceco/gm2/gravitybox/GravityBoxSettings.java
@@ -584,6 +584,7 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
     public static final String EXTRA_SR_ON_DATA_DISABLED = "smartRadioOnDataDisabled";
 
     public static final String PREF_KEY_IME_FULLSCREEN_DISABLE = "pref_ime_fullscreen_disable";
+    public static final String PREF_KEY_TORCH_AUTO_OFF = "pref_torch_auto_off";
 
     private static final int REQ_LOCKSCREEN_BACKGROUND = 1024;
     private static final int REQ_NOTIF_BG_IMAGE_PORTRAIT = 1025;
