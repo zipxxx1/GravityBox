d99e2fdc72b88a09dca54c1deb3442e70973d26e
C3C0 <ceco@ceco.sk.eu.org>
2014-01-20 17:47:27 +0100

    Lockscreen custom image: include decors when computing crop size (KK Only)

--- src/com/ceco/kitkat/gravitybox/GravityBoxSettings.java
+++ src/com/ceco/kitkat/gravitybox/GravityBoxSettings.java
@@ -52,6 +52,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.app.WallpaperManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -66,6 +67,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.Rect;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -2191,33 +2193,28 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
             intent.putExtra("scaleUpIfNeeded", false);
             intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
             Display display = getActivity().getWindowManager().getDefaultDisplay();
-            int width = display.getWidth();
-            int height = display.getHeight();
-            Rect rect = new Rect();
-            Window window = getActivity().getWindow();
-            window.getDecorView().getWindowVisibleDisplayFrame(rect);
-            int statusBarHeight = rect.top;
-            int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
-            int titleBarHeight = contentViewTop - statusBarHeight;
+            Point displaySize = new Point();
+            display.getRealSize(displaySize);
             // Lock screen for tablets visible section are different in landscape/portrait,
             // image need to be cropped correctly, like wallpaper setup for scrolling in background in home screen
             // other wise it does not scale correctly
             if (Utils.isTabletUI(getActivity())) {
-                width = getActivity().getWallpaperDesiredMinimumWidth();
-                height = getActivity().getWallpaperDesiredMinimumHeight();
-                float spotlightX = (float) display.getWidth() / width;
-                float spotlightY = (float) display.getHeight() / height;
-                intent.putExtra("aspectX", width);
-                intent.putExtra("aspectY", height);
-                intent.putExtra("outputX", width);
-                intent.putExtra("outputY", height);
+                WallpaperManager wpManager = WallpaperManager.getInstance(getActivity());
+                int wpWidth = wpManager.getDesiredMinimumWidth();
+                int wpHeight = wpManager.getDesiredMinimumHeight();
+                float spotlightX = (float) displaySize.x / wpWidth;
+                float spotlightY = (float) displaySize.y / wpHeight;
+                intent.putExtra("aspectX", wpWidth);
+                intent.putExtra("aspectY", wpHeight);
+                intent.putExtra("outputX", wpWidth);
+                intent.putExtra("outputY", wpHeight);
                 intent.putExtra("spotlightX", spotlightX);
                 intent.putExtra("spotlightY", spotlightY);
             } else {
                 boolean isPortrait = getResources().getConfiguration().orientation ==
                     Configuration.ORIENTATION_PORTRAIT;
-                intent.putExtra("aspectX", isPortrait ? width : height - titleBarHeight);
-                intent.putExtra("aspectY", isPortrait ? height - titleBarHeight : width);
+                intent.putExtra("aspectX", isPortrait ? displaySize.x : displaySize.y);
+                intent.putExtra("aspectY", isPortrait ? displaySize.y : displaySize.x);
             }
             try {
                 wallpaperTemporary.createNewFile();
