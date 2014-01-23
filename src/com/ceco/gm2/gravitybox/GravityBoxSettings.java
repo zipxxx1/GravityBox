02a0db02db979022e2d8afe1f3f01340300f85e5
C3C0 <pete.gregus@gmail.com>
2014-01-23 11:38:38 +0100

    Notification drawer style: option for custom carrier text

--- src/com/ceco/gm2/gravitybox/GravityBoxSettings.java
+++ src/com/ceco/gm2/gravitybox/GravityBoxSettings.java
@@ -351,16 +351,19 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
     public static final String PREF_KEY_NOTIF_IMAGE_PORTRAIT = "pref_notif_image_portrait";
     public static final String PREF_KEY_NOTIF_IMAGE_LANDSCAPE = "pref_notif_image_landscape";
     public static final String PREF_KEY_NOTIF_BACKGROUND_ALPHA = "pref_notif_background_alpha";
+    public static final String PREF_KEY_NOTIF_CARRIER_TEXT = "pref_notif_carrier_text";
     public static final String NOTIF_BG_DEFAULT = "default";
     public static final String NOTIF_BG_COLOR = "color";
     public static final String NOTIF_BG_IMAGE = "image";
     public static final String NOTIF_BG_COLOR_MODE_OVERLAY = "overlay";
     public static final String NOTIF_BG_COLOR_MODE_UNDERLAY = "underlay";
     public static final String ACTION_NOTIF_BACKGROUND_CHANGED = "gravitybox.intent.action.NOTIF_BACKGROUND_CHANGED";
+    public static final String ACTION_NOTIF_CARRIER_TEXT_CHANGED = "gravitybox.intent.action.NOTIF_CARRIER_TEXT_CHANGED";
     public static final String EXTRA_BG_TYPE = "bgType";
     public static final String EXTRA_BG_COLOR = "bgColor";
     public static final String EXTRA_BG_COLOR_MODE = "bgColorMode";
     public static final String EXTRA_BG_ALPHA = "bgAlpha";
+    public static final String EXTRA_NOTIF_CARRIER_TEXT = "notifCarrierText";
 
     public static final String PREF_KEY_PIE_CONTROL_ENABLE = "pref_pie_control_enable2";
     public static final String PREF_KEY_PIE_CONTROL_CUSTOM_KEY = "pref_pie_control_custom_key";
@@ -850,6 +853,7 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
         private Preference mPrefNotifImageLandscape;
         private ListPreference mPrefNotifColorMode;
         private CheckBoxPreference mPrefDisableDataNetworkTypeIcons;
+        private EditTextPreference mPrefNotifCarrierText;
         private CheckBoxPreference mPrefDisableRoamingIndicators;
         private ListPreference mPrefButtonBacklightMode;
         private ListPreference mPrefPieEnabled;
@@ -1064,6 +1068,7 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
             mPrefNotifImagePortrait = (Preference) findPreference(PREF_KEY_NOTIF_IMAGE_PORTRAIT);
             mPrefNotifImageLandscape = (Preference) findPreference(PREF_KEY_NOTIF_IMAGE_LANDSCAPE);
             mPrefNotifColorMode = (ListPreference) findPreference(PREF_KEY_NOTIF_COLOR_MODE);
+            mPrefNotifCarrierText = (EditTextPreference) findPreference(PREF_KEY_NOTIF_CARRIER_TEXT);
 
             mPrefDisableDataNetworkTypeIcons = (CheckBoxPreference) findPreference(PREF_KEY_DISABLE_DATA_NETWORK_TYPE_ICONS);
             mPrefDisableRoamingIndicators = (CheckBoxPreference) findPreference(PREF_KEY_DISABLE_ROAMING_INDICATORS);
@@ -1812,6 +1817,16 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
                 mPrefLockscreenCarrierText.setSummary(carrierText);
             }
 
+            if (key == null || key.equals(PREF_KEY_NOTIF_CARRIER_TEXT)) {
+                String carrierText = mPrefNotifCarrierText.getText();
+                if (carrierText == null || carrierText.isEmpty()) {
+                    carrierText = getString(R.string.carrier_text_default);
+                } else if (carrierText.trim().isEmpty()) {
+                    carrierText = getString(R.string.carrier_text_empty);
+                }
+                mPrefNotifCarrierText.setSummary(carrierText);
+            }
+
             if (key == null || key.equals(PREF_KEY_PIE_BACK_LONGPRESS)) {
                 mPrefPieBackLongpress.setSummary(mPrefPieBackLongpress.getEntry());
             }
@@ -2061,6 +2076,10 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
                 intent.setAction(ACTION_DISABLE_DATA_NETWORK_TYPE_ICONS_CHANGED);
                 intent.putExtra(EXTRA_DATA_NETWORK_TYPE_ICONS_DISABLED,
                         prefs.getBoolean(PREF_KEY_DISABLE_DATA_NETWORK_TYPE_ICONS, false));
+            } else if (key.equals(PREF_KEY_NOTIF_CARRIER_TEXT)) {
+                intent.setAction(ACTION_NOTIF_CARRIER_TEXT_CHANGED);
+                intent.putExtra(EXTRA_NOTIF_CARRIER_TEXT,
+                        prefs.getString(PREF_KEY_NOTIF_CARRIER_TEXT, null));
             } else if (key.equals(PREF_KEY_DISABLE_ROAMING_INDICATORS)) {
                 intent.setAction(ACTION_DISABLE_ROAMING_INDICATORS_CHANGED);
                 intent.putExtra(EXTRA_INDICATORS_DISABLED,
