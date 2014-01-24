f5901a6baafa3cada6e2895c61aea146cf61a820
C3C0 <pete.gregus@gmail.com>
2014-01-24 10:52:13 +0100

    Smart Radio: added option for delaying network mode change Useful to filter out quick changes caused by user interaction
    Explanation for using partial WakeLock:
    There's one special case that requires utilization of partial WakeLock.
    When delay is set to other value than 0 and screen turns off we have to
    make sure CPU is online until mode is changed to POWER_SAVING, otherwise
    change in network mode would not be handled.
    WakeLock is acquired for the specific amount of time based on delay
    preference thus it will be automatically released by the system in case
    something went wrong that prevented us from releasing it ourselves.

--- src/com/ceco/kitkat/gravitybox/ModSmartRadio.java
+++ src/com/ceco/kitkat/gravitybox/ModSmartRadio.java
@@ -21,6 +21,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
@@ -47,6 +50,8 @@ public class ModSmartRadio {
     private static boolean mIsScreenOff;
     private static boolean mPowerSaveWhenScreenOff;
     private static boolean mIgnoreWhileLocked;
+    private static NetworkModeChanger mNetworkModeChanger;
+    private static int mModeChangeDelay;
 
     private static BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -70,6 +75,10 @@ public class ModSmartRadio {
                     mIgnoreWhileLocked = intent.getBooleanExtra(GravityBoxSettings.EXTRA_SR_IGNORE_LOCKED, true);
                     if (DEBUG) log("mIgnoreWhileLocked = " + mIgnoreWhileLocked);
                 }
+                if (intent.hasExtra(GravityBoxSettings.EXTRA_SR_MODE_CHANGE_DELAY)) {
+                    mModeChangeDelay = intent.getIntExtra(GravityBoxSettings.EXTRA_SR_MODE_CHANGE_DELAY, 5);
+                    if (DEBUG) log("mModeChangeDelay = " + mModeChangeDelay);
+                }
             } else if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
                 boolean mobileDataEnabled = isMobileDataEnabled(); 
                 boolean mobileNetworkAvailable = isMobileNetworkAvailable();
@@ -92,10 +101,17 @@ public class ModSmartRadio {
                     } else if (mWasMobileDataEnabled && !mobileDataEnabled) {
                         if (DEBUG) log("Mobile data got disabled");
                         switchToState(State.POWER_SAVING);
-                    } else if (!mWasMobileNetworkAvailable && mobileNetworkAvailable &&
-                            mobileDataEnabled && !wifiConnected) {
-                        if (DEBUG) log("Mobile network got available and data active");
-                        switchToState(State.NORMAL, true);
+                    } else if (!mWasMobileNetworkAvailable && mobileNetworkAvailable) {
+                        if (mobileDataEnabled && !wifiConnected) {
+                            if (DEBUG) log("Mobile network got available and data active");
+                            switchToState(State.NORMAL, true);
+                        } else {
+                            if (DEBUG) log("Mobile network got available but data not active");
+                            switchToState(State.POWER_SAVING, true);
+                        }
+                    } else if (mWasMobileNetworkAvailable && !mobileNetworkAvailable) {
+                        if (DEBUG) log("Mobile network unavailable. Resetting state to POWER_SAVING.");
+                        switchToState(State.POWER_SAVING);
                     }
                     mWasMobileDataEnabled = mobileDataEnabled;
                     mWasMobileNetworkAvailable = mobileNetworkAvailable;
@@ -103,7 +119,7 @@ public class ModSmartRadio {
             } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
                 if (DEBUG) log("Screen turning off");
                 if (mPowerSaveWhenScreenOff) {
-                    switchToState(State.POWER_SAVING, true);
+                    switchToState(State.POWER_SAVING, true, true);
                 }
                 mIsScreenOff = true;
             } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
@@ -146,16 +162,22 @@ public class ModSmartRadio {
     }
 
     private static void switchToState(State newState) {
-        switchToState(newState, false);
+        switchToState(newState, false, false);
     }
 
     private static void switchToState(State newState, boolean force) {
+        switchToState(newState, force, false);
+    }
+
+    private static void switchToState(State newState, boolean force, boolean withWakeLock) {
         if (mCurrentState == newState && !force) {
             if (DEBUG) log("switchToState: new state == previous state - ignoring");
             return;
         } else if (!isMobileNetworkAvailable()) {
-            if (DEBUG) log("switchToState: mobile network unavailable - ignoring");
-            return;
+            // force power saving state no matter what so we start with it when mobile network is available again
+            if (DEBUG) log("switchToState: mobile network unavailable - resetting to POWER_SAVING state");
+            mWasMobileNetworkAvailable = false;
+            newState = State.POWER_SAVING;
         } else if (DEBUG) {
             log("Switching to state: " + newState);
         }
@@ -167,11 +189,7 @@ public class ModSmartRadio {
                 case POWER_SAVING: networkMode = mPowerSavingMode; break;
                 default: break;
             }
-            if (networkMode != -1) {
-                Intent intent = new Intent(PhoneWrapper.ACTION_CHANGE_NETWORK_TYPE);
-                intent.putExtra(PhoneWrapper.EXTRA_NETWORK_TYPE, networkMode);
-                mContext.sendBroadcast(intent);
-            }
+            mNetworkModeChanger.changeNetworkMode(networkMode, withWakeLock);
             mCurrentState = newState;
         } catch (Throwable t) {
             log("switchToState: " + t.getMessage());
@@ -192,6 +210,59 @@ public class ModSmartRadio {
         }
     }
 
+    private static class NetworkModeChanger implements Runnable {
+        private Context mContext;
+        private Handler mHandler;
+        private int mNextNetworkMode;
+        private int mCurrentNetworkMode;
+        private WakeLock mWakeLock;
+
+        public NetworkModeChanger(Context context) {
+            mContext = context;
+            mHandler = new Handler();
+            mNextNetworkMode = -1;
+            mCurrentNetworkMode = -1;
+
+            PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "GB:SmartRadio");
+        }
+
+        @Override
+        public void run() {
+            if (mContext == null || mNextNetworkMode == -1) return;
+            if (DEBUG) log("NetworkModeChanger: sending intent");
+            Intent intent = new Intent(PhoneWrapper.ACTION_CHANGE_NETWORK_TYPE);
+            intent.putExtra(PhoneWrapper.EXTRA_NETWORK_TYPE, mNextNetworkMode);
+            mContext.sendBroadcast(intent);
+            mCurrentNetworkMode = mNextNetworkMode;
+            releaseWakeLockIfHeld();
+        }
+
+        public void changeNetworkMode(int networkMode, boolean withWakeLock) {
+            mHandler.removeCallbacks(this);
+            releaseWakeLockIfHeld();
+            if (networkMode == -1 || networkMode == mCurrentNetworkMode) return;
+            mNextNetworkMode = networkMode;
+            if (mModeChangeDelay == 0) {
+                run();
+            } else {
+                if (DEBUG) log("NetworkModeChanger: scheduling network mode change");
+                if (withWakeLock) {
+                    mWakeLock.acquire(mModeChangeDelay*1000+1000);
+                    if (DEBUG) log("NetworkModeChanger: Wake Lock acquired");
+                }
+                mHandler.postDelayed(this, mModeChangeDelay*1000);
+            }
+        }
+
+        private void releaseWakeLockIfHeld() {
+            if (mWakeLock != null && mWakeLock.isHeld()) {
+                mWakeLock.release();
+                if (DEBUG) log("NetworkModeChanger: Wake Lock released");
+            }
+        }
+    }
+
     public static void init(final XSharedPreferences prefs, final ClassLoader classLoader) {
         try {
             final Class<?> classSystemUIService = XposedHelpers.findClass(
@@ -201,6 +272,7 @@ public class ModSmartRadio {
             mPowerSavingMode = prefs.getInt(GravityBoxSettings.PREF_KEY_SMART_RADIO_POWER_SAVING_MODE, -1);
             mPowerSaveWhenScreenOff = prefs.getBoolean(GravityBoxSettings.PREF_KEY_SMART_RADIO_SCREEN_OFF, false);
             mIgnoreWhileLocked = prefs.getBoolean(GravityBoxSettings.PREF_KEY_SMART_RADIO_IGNORE_LOCKED, true);
+            mModeChangeDelay = prefs.getInt(GravityBoxSettings.PREF_KEY_SMART_RADIO_MODE_CHANGE_DELAY, 5);
 
             XposedHelpers.findAndHookMethod(classSystemUIService, "onCreate", new XC_MethodHook() {
                 @Override
@@ -210,6 +282,7 @@ public class ModSmartRadio {
                         if (DEBUG) log("Initializing SmartRadio");
 
                         mConnManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+                        mNetworkModeChanger = new NetworkModeChanger(mContext);
 
                         IntentFilter intentFilter = new IntentFilter();
                         intentFilter.addAction(GravityBoxSettings.ACTION_PREF_SMART_RADIO_CHANGED);
