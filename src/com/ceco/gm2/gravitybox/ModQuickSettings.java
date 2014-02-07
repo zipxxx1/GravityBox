0fc8e1be5f1bbd5bb4efdbac593f316fc38a3304
C3C0 <pete.gregus@gmail.com>
2014-02-07 16:04:48 +0100

    ModQuickSettings: Auto-switch QS option extended Two modes: 1) auto-switch to quick settings when there are no notifications 2) auto-switch to quick settings when there are no CLEARABLE notifications (e.g. will switch to QS even if there are ongoing notifications)

--- src/com/ceco/gm2/gravitybox/ModQuickSettings.java
+++ src/com/ceco/gm2/gravitybox/ModQuickSettings.java
@@ -112,7 +112,7 @@ public class ModQuickSettings {
     private static Object mSimSwitchPanelView;
     private static int mNumColumns = 3;
     private static int mLpOriginalHeight = -1;
-    private static boolean mAutoSwitch = false;
+    private static int mAutoSwitch;
     private static int mQuickPulldown = GravityBoxSettings.QUICK_PULLDOWN_OFF;
     private static Method methodGetColumnSpan;
     private static List<String> mCustomSystemTileKeys;
@@ -215,8 +215,7 @@ public class ModQuickSettings {
                     }
                 }
                 if (intent.hasExtra(GravityBoxSettings.EXTRA_QS_AUTOSWITCH)) {
-                    mAutoSwitch = intent.getBooleanExtra(
-                            GravityBoxSettings.EXTRA_QS_AUTOSWITCH, false);
+                    mAutoSwitch = intent.getIntExtra(GravityBoxSettings.EXTRA_QS_AUTOSWITCH, 0);
                 }
                 if (intent.hasExtra(GravityBoxSettings.EXTRA_QUICK_PULLDOWN)) {
                     mQuickPulldown = intent.getIntExtra(
@@ -441,7 +440,8 @@ public class ModQuickSettings {
                 log("Invalid preference for tiles per row: " + e.getMessage());
             }
 
-            mAutoSwitch = mPrefs.getBoolean(GravityBoxSettings.PREF_KEY_QUICK_SETTINGS_AUTOSWITCH, false);
+            mAutoSwitch = Integer.valueOf(
+                    mPrefs.getString(GravityBoxSettings.PREF_KEY_QUICK_SETTINGS_AUTOSWITCH, "0"));
             mHideOnChange = mPrefs.getBoolean(GravityBoxSettings.PREF_KEY_QUICK_SETTINGS_HIDE_ON_CHANGE, false);
             mQsTileSpanDisable = mPrefs.getBoolean(GravityBoxSettings.PREF_KEY_QS_TILE_SPAN_DISABLE, false);
 
@@ -730,8 +730,10 @@ public class ModQuickSettings {
                                     mGestureStartY > height - handleBarHeight - paddingBottom;
                             okToFlip = (expandedHeight == 0);
                             XposedHelpers.setBooleanField(param.thisObject, "mOkToFlip", okToFlip);
-                            if (mAutoSwitch && 
-                                    (Integer)XposedHelpers.callMethod(notificationData, "size") == 0 &&
+                            if (mAutoSwitch == 1 && !notifDataHasVisibleItems(notificationData) &&
+                                    !isSimSwitchPanelShowing()) {
+                                shouldFlip = true;
+                            } else if (mAutoSwitch == 2 && !notifDataHasClearableItems(notificationData) &&
                                     !isSimSwitchPanelShowing()) {
                                 shouldFlip = true;
                             } else if (mQuickPulldown == GravityBoxSettings.QUICK_PULLDOWN_RIGHT
@@ -835,6 +837,39 @@ public class ModQuickSettings {
             }
         }
     };
+
+    private static boolean notifDataHasVisibleItems(Object notifData) {
+        try {
+            ArrayList<Object> entries = (ArrayList<Object>) XposedHelpers.getObjectField(notifData, "mEntries");
+            for (Object e : entries) {
+                if (XposedHelpers.getObjectField(e, "expanded") != null) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+            return false;
+        }
+    }
+
+    private static boolean notifDataHasClearableItems(Object notifData) {
+        try {
+            ArrayList<Object> entries = (ArrayList<Object>) XposedHelpers.getObjectField(notifData, "mEntries");
+            for (Object e : entries) {
+                if (XposedHelpers.getObjectField(e, "expanded") != null) {
+                    Object notif = XposedHelpers.getObjectField(e, "notification");
+                    if ((Boolean)XposedHelpers.callMethod(notif, "isClearable")) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+            return false;
+        }
+    }
 
     private static Object getObj(String name) {
         return XposedHelpers.getObjectField(mStatusBar, name);
