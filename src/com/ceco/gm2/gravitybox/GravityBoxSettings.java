500249a05375ff313607890dc68af277bca5bf47
Bruno Martins <bgcngm@gmail.com>
2014-02-08 16:04:26 +0000

    Key actions: hide action for expanding QS on Android 4.1

--- src/com/ceco/gm2/gravitybox/GravityBoxSettings.java
+++ src/com/ceco/gm2/gravitybox/GravityBoxSettings.java
@@ -1455,6 +1455,10 @@ public class GravityBoxSettings extends Activity implements GravityBoxResultRece
                 actEntries.remove(getString(R.string.hwkey_action_torch));
                 actEntryValues.remove("11");
             }
+            if (Build.VERSION.SDK_INT < 17) {
+                actEntries.remove(getString(R.string.hwkey_action_expand_quicksettings));
+                actEntryValues.remove("18");
+            }
             if (!(Utils.isAppInstalled(getActivity(), APP_GOOGLE_NOW) &&
                     Utils.isAppInstalled(getActivity(), APP_GOOGLE_HOME))) {
                 actEntries.remove(getString(R.string.hwkey_action_launcher_drawer));
