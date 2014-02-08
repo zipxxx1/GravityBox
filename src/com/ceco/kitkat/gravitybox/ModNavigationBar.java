bc33b7552f72550fed6e4885098896ad6f8401ee
C3C0 <ceco@ceco.sk.eu.org>
2014-02-08 18:36:25 +0100

    Navbar ring: enable haptic feedback only in case it's not by default

--- src/com/ceco/kitkat/gravitybox/ModNavigationBar.java
+++ src/com/ceco/kitkat/gravitybox/ModNavigationBar.java
@@ -511,8 +511,10 @@ public class ModNavigationBar {
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         prefs.reload();
                         mGlowPadView = (View) XposedHelpers.getObjectField(param.thisObject, "mGlowPadView");
-                        XposedHelpers.setIntField(mGlowPadView, "mVibrationDuration", 20);
-                        XposedHelpers.callMethod(mGlowPadView, "setVibrateEnabled", true);
+                        if (XposedHelpers.getIntField(mGlowPadView, "mVibrationDuration") == 0) {
+                            XposedHelpers.setIntField(mGlowPadView, "mVibrationDuration", 20);
+                            XposedHelpers.callMethod(mGlowPadView, "setVibrateEnabled", true);
+                        }
                         setRingTargets();
                     }
                 });
