8ddbf63ea9c16c35bb52732f463ee7606b0dda44
C3C0 <ceco@ceco.sk.eu.org>
2014-05-21 15:35:20 +0200

    More strict isPhoneUI check Fixes issues on Nexus 7 caused by device UI not properly identified

--- src/com/ceco/gm2/gravitybox/Utils.java
+++ src/com/ceco/gm2/gravitybox/Utils.java
@@ -129,7 +129,7 @@ public class Utils {
     }
 
     public static boolean isPhoneUI(Context con) {
-        return getScreenType(con) == DEVICE_PHONE;
+        return (getScreenType(con) == DEVICE_PHONE && !isTablet());
     }
 
     public static boolean isHybridUI(Context con) {
