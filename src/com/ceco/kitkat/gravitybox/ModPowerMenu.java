0c3a9636867cd3406e2ebb9ef4597bfeb3088de8
C3C0 <ceco@ceco.sk.eu.org>
2014-08-30 20:35:40 +0200

    PowerMenu: apply "disable reboot on lockscreen" to stock reboot item Previously, it applied only to reboot item that was injected by us.

--- src/com/ceco/kitkat/gravitybox/ModPowerMenu.java
+++ src/com/ceco/kitkat/gravitybox/ModPowerMenu.java
@@ -83,6 +83,7 @@ public class ModPowerMenu {
     private static String mScreenshotStr;
     private static String mScreenrecordStr;
     private static Unhook mRebootActionHook;
+    private static Unhook mRebootActionShowHook;
     private static Object mRebootActionItem;
     private static boolean mRebootActionItemStockExists;
     private static Object mScreenshotAction;
@@ -182,6 +183,10 @@ public class ModPowerMenu {
                         mRebootActionHook.unhook();
                         mRebootActionHook = null;
                     }
+                    if (mRebootActionShowHook != null) {
+                        mRebootActionShowHook.unhook();
+                        mRebootActionShowHook = null;
+                    }
                 }
 
                 @Override
@@ -263,6 +268,13 @@ public class ModPowerMenu {
                                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
                                     RebootAction.showRebootDialog(mContext);
                                     return null;
+                                }
+                            });
+                            mRebootActionShowHook = XposedHelpers.findAndHookMethod(mRebootActionItem.getClass(),
+                                    "showDuringKeyguard", new XC_MethodReplacement() {
+                                @Override
+                                protected Object replaceHookedMethod(MethodHookParam param)throws Throwable {
+                                    return mRebootAllowOnLockscreen;
                                 }
                             });
                         } else {
