48fae45ac259e52af0e20af1cf4c931a2dc5c4ff
C3C0 <ceco@ceco.sk.eu.org>
2015-02-15 10:56:03 +0100

    Fixed volume keys skip track feature

--- src/com/ceco/lollipop/gravitybox/ModVolumeKeySkipTrack.java
+++ src/com/ceco/lollipop/gravitybox/ModVolumeKeySkipTrack.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 rovo89@xda
- * Copyright (C) 2013 Peter Gregus for GravityBox Project (C3C076@xda)
+ * Copyright (C) 2015 Peter Gregus for GravityBox Project (C3C076@xda)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,7 +16,6 @@
 
 package com.ceco.lollipop.gravitybox;
 
-import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getAdditionalInstanceField;
@@ -24,21 +23,23 @@ import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static de.robv.android.xposed.XposedHelpers.setAdditionalInstanceField;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.SystemClock;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 
 public class ModVolumeKeySkipTrack {
     private static final String TAG = "GB:ModVolumeKeySkipTrack";
     private static final boolean DEBUG = false;
 
     private static boolean mIsLongPress = false;
-    private static boolean allowSkipTrack; 
+    private static boolean allowSkipTrack;
+    private static AudioManager mAudioManager;
 
     private static void log(String message) {
         XposedBridge.log(TAG + ": " + message);
@@ -54,7 +55,7 @@ public class ModVolumeKeySkipTrack {
             XposedBridge.hookAllConstructors(classPhoneWindowManager, handleConstructPhoneWindowManager);
 
             // take advantage of screenTurnedOff method for refreshing state of allowSkipTrack preference
-            findAndHookMethod(classPhoneWindowManager, "screenTurnedOff", int.class, new XC_MethodHook() {
+            findAndHookMethod(classPhoneWindowManager, "screenTurnedOff", new XC_MethodHook() {
 
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -64,19 +65,25 @@ public class ModVolumeKeySkipTrack {
             });
 
             findAndHookMethod(classPhoneWindowManager, "interceptKeyBeforeQueueing",
-                    KeyEvent.class, int.class, boolean.class, handleInterceptKeyBeforeQueueing);
+                    KeyEvent.class, int.class, handleInterceptKeyBeforeQueueing);
         } catch (Throwable t) { XposedBridge.log(t); }
     }
 
     private static XC_MethodHook handleInterceptKeyBeforeQueueing = new XC_MethodHook() {
         @Override
         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-            final boolean isScreenOn = (Boolean) param.args[2];
+            final boolean isScreenOn = XposedHelpers.getBooleanField(param.thisObject, "mScreenOnFully");
             if (!isScreenOn && allowSkipTrack) { 
+                if (mAudioManager == null) {
+                    Context context = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
+                    if (context != null) {
+                        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                    }
+                }
                 final KeyEvent event = (KeyEvent) param.args[0];
                 final int keyCode = event.getKeyCode();
                 if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP)
-                        && (Boolean) callMethod(param.thisObject, "isMusicActive") == true) {
+                        && mAudioManager != null && mAudioManager.isMusicActive()) {
                     if (event.getAction() == KeyEvent.ACTION_DOWN) {
                         mIsLongPress = false;
                         handleVolumeLongPress(param.thisObject, keyCode);
@@ -91,10 +98,9 @@ public class ModVolumeKeySkipTrack {
 
                         // send an additional "key down" because the first one was eaten
                         // the "key up" is what we are just processing
-                        Object[] newArgs = new Object[3];
+                        Object[] newArgs = new Object[2];
                         newArgs[0] = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
                         newArgs[1] = param.args[1];
-                        newArgs[2] = param.args[2];
                         XposedBridge.invokeOriginalMethod(param.method, param.thisObject, newArgs);
                     }
                 }
@@ -150,34 +156,9 @@ public class ModVolumeKeySkipTrack {
         dispatchMediaButtonEvent(keyEvent);
     }
 
-    /*
-     * Attempt to execute the following with reflection. 
-     * 
-     * [Code]
-     * IAudioService audioService = IAudioService.Stub.asInterface(b);
-     * audioService.dispatchMediaKeyEvent(keyEvent);
-     * This seems to work correctly with Google Play Music on 4.3
-     * And from looking at the AOSP source they started doing it this way
-     * in 4.1.1
-     *
-     * See: https://android.googlesource.com/platform/frameworks/base.git/+/android-4.1.1_r1.1/policy/src/com/android/internal/policy/impl/KeyguardViewBase.java
-     */
     private static void dispatchMediaButtonEvent(KeyEvent keyEvent) {
         try {
-            IBinder iBinder = (IBinder) Class.forName("android.os.ServiceManager")
-                    .getDeclaredMethod("checkService", String.class)
-                    .invoke(null, Context.AUDIO_SERVICE);
-            if (DEBUG ) log("Got Binder");
-
-            // get audioService from IAudioService.Stub.asInterface(IBinder)
-            Object audioService  = Class.forName("android.media.IAudioService$Stub")
-                    .getDeclaredMethod("asInterface",IBinder.class)
-                    .invoke(null,iBinder);
-
-            // Dispatch keyEvent using IAudioService.dispatchMediaKeyEvent(KeyEvent)
-            Class.forName("android.media.IAudioService")
-                    .getDeclaredMethod("dispatchMediaKeyEvent",KeyEvent.class)
-                    .invoke(audioService, keyEvent);
+            mAudioManager.dispatchMediaKeyEvent(keyEvent);
         } catch (Throwable t) {
             XposedBridge.log(t);
         }
