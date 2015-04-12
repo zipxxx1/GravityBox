a344f8b2b56ae2f2d3eed0a3703b181f56615a01
C3C0 <ceco@ceco.sk.eu.org>
2015-04-12 11:55:18 +0200

    Launcher: updated for compatibility with Google App 4.4.10

--- src/com/ceco/lollipop/gravitybox/ModLauncher.java
+++ src/com/ceco/lollipop/gravitybox/ModLauncher.java
@@ -82,6 +82,7 @@ public class ModLauncher {
         CLASS_DYNAMIC_GRID.add(new DynamicGrid("wd", "Fe", "Ce", "Cf"));
         CLASS_DYNAMIC_GRID.add(new DynamicGrid("com.android.launcher3.cn", "KA", "Hz", "HA"));
         CLASS_DYNAMIC_GRID.add(new DynamicGrid("com.android.launcher3.cn", "Kz", "Hy", "Hz"));
+        CLASS_DYNAMIC_GRID.add(new DynamicGrid("com.android.launcher3.cn", "KE", "HD", "HE"));
 
         METHOD_SHOW_ALL_APPS = new ArrayList<ShowAllApps>();
         METHOD_SHOW_ALL_APPS.add(new ShowAllApps("onClickAllAppsButton",
@@ -96,6 +97,9 @@ public class ModLauncher {
         METHOD_SHOW_ALL_APPS.add(new ShowAllApps("a",
                 new Object[] { boolean.class, "com.android.launcher3.q", boolean.class },
                 new Object[] { false, "Db", false } ));
+        METHOD_SHOW_ALL_APPS.add(new ShowAllApps("a",
+                new Object[] { boolean.class, "com.android.launcher3.q", boolean.class },
+                new Object[] { false, "Dg", false } ));
     }
 
     private static void log(String message) {
