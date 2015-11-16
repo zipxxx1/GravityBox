2c45097a4e2d0ec50a0339bfe876e994365156fa
C3C0 <ceco@ceco.sk.eu.org>
2015-11-16 14:10:20 +0100

    QS Tiles: added support for AOSP DND tile

--- src/com/ceco/marshmallow/gravitybox/quicksettings/AospTile.java
+++ src/com/ceco/marshmallow/gravitybox/quicksettings/AospTile.java
@@ -40,6 +40,8 @@ public abstract class AospTile extends BaseTile implements QsEventListener {
             return new RotationLockTile(host, tile, prefs, eventDistributor);
         else if (WifiTile.AOSP_KEY.equals(aospKey))
             return new WifiTile(host, tile, prefs, eventDistributor);
+        else if (DoNotDisturbTile.AOSP_KEY.equals(aospKey))
+            return new DoNotDisturbTile(host, tile, prefs, eventDistributor);
         else if (MtkAudioProfileTile.AOSP_KEY.equals(aospKey))
             return new MtkAudioProfileTile(host, tile, prefs, eventDistributor);
         else if (MtkMobileDataTile.AOSP_KEY.equals(aospKey))
---
+++ src/com/ceco/marshmallow/gravitybox/quicksettings/DoNotDisturbTile.java
@@ -0,0 +1,22 @@
+package com.ceco.marshmallow.gravitybox.quicksettings;
+
+import de.robv.android.xposed.XSharedPreferences;
+
+public class DoNotDisturbTile extends AospTile {
+    public static final String AOSP_KEY = "dnd";
+
+    protected DoNotDisturbTile(Object host, Object tile, XSharedPreferences prefs,
+            QsTileEventDistributor eventDistributor) throws Throwable {
+        super(host, "aosp_tile_dnd", tile, prefs, eventDistributor);
+    }
+
+    @Override
+    protected String getClassName() {
+        return "com.android.systemui.qs.tiles.DndTile";
+    }
+
+    @Override
+    public String getAospKey() {
+        return AOSP_KEY;
+    }
+}
