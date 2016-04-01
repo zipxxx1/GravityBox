bba3d28b9db460d07659928a72104c863cc447dc
C3C0 <ceco@ceco.sk.eu.org>
2016-04-01 14:02:12 +0200

    Navbar: couple of auto-fade adjustments

--- src/com/ceco/marshmallow/gravitybox/ModNavigationBar.java
+++ src/com/ceco/marshmallow/gravitybox/ModNavigationBar.java
@@ -194,8 +194,8 @@ public class ModNavigationBar {
                 }
                 if (intent.hasExtra(GravityBoxSettings.EXTRA_NAVBAR_AUTOFADE_KEYS)) {
                     mAutofadeTimeoutMs = intent.getIntExtra(GravityBoxSettings.EXTRA_NAVBAR_AUTOFADE_KEYS, 0) * 1000;
+                    mBarModeHandler.removeMessages(MSG_LIGHTS_OUT);
                     if (mAutofadeTimeoutMs == 0) {
-                        mBarModeHandler.removeMessages(MSG_LIGHTS_OUT);
                         setBarMode(mBarModeOriginal);
                     } else {
                         mBarModeHandler.sendEmptyMessageDelayed(MSG_LIGHTS_OUT, mAutofadeTimeoutMs);
@@ -498,8 +498,8 @@ public class ModNavigationBar {
                     }
                     if (mAutofadeTimeoutMs > 0 &&
                             SystemClock.uptimeMillis() - mLastTouchMs >= mAutofadeTimeoutMs &&
-                            mBarModeOriginal != MODE_LIGHTS_OUT &&
-                            mBarModeOriginal != MODE_LIGHTS_OUT_TRANSPARENT) {
+                                barMode != MODE_LIGHTS_OUT &&
+                                barMode != MODE_LIGHTS_OUT_TRANSPARENT) {
                         param.args[0] = MODE_LIGHTS_OUT_TRANSPARENT;
                     }
                 }
