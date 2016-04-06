7de58bce7887597baf3c8fbdb5b861b3c99d823f
C3C0 <ceco@ceco.sk.eu.org>
2016-04-06 19:53:50 +0200

    Lockscreen: use dedicated try/catch for carrier text feature Implementation often seems to differ across different devices/systems

--- src/com/ceco/lollipop/gravitybox/ModLockscreen.java
+++ src/com/ceco/lollipop/gravitybox/ModLockscreen.java
@@ -453,27 +453,31 @@ public class ModLockscreen {
             });
 
             if (!Utils.isXperiaDevice()) {
-                XC_MethodHook carrierTextHook = new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                        String text = mPrefs.getString(GravityBoxSettings.PREF_KEY_LOCKSCREEN_CARRIER_TEXT, "");
-                        if (mCarrierTextView == null) {
-                            mCarrierTextView = (TextView) param.thisObject;
-                        } 
-                        if (text.isEmpty()) {
-                            return;
-                        } else {
-                            mCarrierTextView.setText(text.trim().isEmpty() ? "" : text);
-                            param.setResult(null);
+                try {
+                    XC_MethodHook carrierTextHook = new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
+                            String text = mPrefs.getString(GravityBoxSettings.PREF_KEY_LOCKSCREEN_CARRIER_TEXT, "");
+                            if (mCarrierTextView == null) {
+                                mCarrierTextView = (TextView) param.thisObject;
+                            } 
+                            if (text.isEmpty()) {
+                                return;
+                            } else {
+                                mCarrierTextView.setText(text.trim().isEmpty() ? "" : text);
+                                param.setResult(null);
+                            }
                         }
+                    };
+                    if (Build.VERSION.SDK_INT < 22) {
+                        XposedHelpers.findAndHookMethod(CLASS_CARRIER_TEXT, classLoader, "updateCarrierText",
+                            CLASS_ICC_STATE, CharSequence.class, CharSequence.class, carrierTextHook);
+                    } else {
+                        XposedHelpers.findAndHookMethod(CLASS_CARRIER_TEXT, classLoader, "updateCarrierText",
+                                carrierTextHook);
                     }
-                };
-                if (Build.VERSION.SDK_INT < 22) {
-                    XposedHelpers.findAndHookMethod(CLASS_CARRIER_TEXT, classLoader, "updateCarrierText",
-                        CLASS_ICC_STATE, CharSequence.class, CharSequence.class, carrierTextHook);
-                } else {
-                    XposedHelpers.findAndHookMethod(CLASS_CARRIER_TEXT, classLoader, "updateCarrierText",
-                            carrierTextHook);
+                } catch (Throwable t) {
+                    log("Unexpected carrier text implementation");
                 }
             }
         } catch (Throwable t) {
