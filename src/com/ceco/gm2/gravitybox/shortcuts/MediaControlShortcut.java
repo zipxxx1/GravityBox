0d7c948ec93060c6b143e9b839f9b94c186d4309
C3C0 <ceco@ceco.sk.eu.org>
2016-04-13 11:20:10 +0200

    Actions: reworked for abstract handling of multi-item shortcuts
    - Introduce abstract AMultiShortcut which implements common
    functionality for all actions having multiple items
    - Introduce ShortcutItem which is a common shortcut item that has label,
    icon and allows specifying ExtraDelegate which is responsible for adding
    action-specific extras to launching intent
    - Changed all existing multi-item shortcuts to be derived from
    AMultiShortcut
    # Conflicts:
    #	src/com/ceco/kitkat/gravitybox/shortcuts/LocationModeShortcut.java
    #	src/com/ceco/kitkat/gravitybox/shortcuts/MediaControlShortcut.java
    #	src/com/ceco/kitkat/gravitybox/shortcuts/NetworkModeShortcut.java
    #	src/com/ceco/kitkat/gravitybox/shortcuts/QuietHoursShortcut.java
    #	src/com/ceco/kitkat/gravitybox/shortcuts/RingerModeShortcut.java
    
    # Conflicts:
    #	src/com/ceco/gm2/gravitybox/shortcuts/MediaControlShortcut.java
    #	src/com/ceco/gm2/gravitybox/shortcuts/NetworkModeShortcut.java
    #	src/com/ceco/gm2/gravitybox/shortcuts/QuietHoursShortcut.java
    #	src/com/ceco/gm2/gravitybox/shortcuts/RingerModeShortcut.java
    #	src/com/ceco/kitkat/gravitybox/shortcuts/LocationModeShortcut.java

--- src/com/ceco/gm2/gravitybox/shortcuts/MediaControlShortcut.java
+++ src/com/ceco/gm2/gravitybox/shortcuts/MediaControlShortcut.java
@@ -21,17 +21,13 @@ import java.util.List;
 import com.ceco.gm2.gravitybox.ModHwKeys;
 import com.ceco.gm2.gravitybox.R;
 import com.ceco.gm2.gravitybox.adapters.IIconListAdapterItem;
-import com.ceco.gm2.gravitybox.adapters.IconListAdapter;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.Intent.ShortcutIconResource;
 import android.graphics.drawable.Drawable;
 import android.view.KeyEvent;
 
-public class MediaControlShortcut extends AShortcut {
+public class MediaControlShortcut extends AMultiShortcut {
     protected static final String ACTION =  ModHwKeys.ACTION_MEDIA_CONTROL;
 
     public MediaControlShortcut(Context context) {
@@ -59,51 +55,34 @@ public class MediaControlShortcut extends AShortcut {
     }
 
     @Override
-    protected ShortcutIconResource getIconResource() {
-        return null;
-    }
-
-    @Override
-    protected void createShortcut(final CreateShortcutListener listener) {
+    protected List<IIconListAdapterItem> getShortcutList() {
         final List<IIconListAdapterItem> list = new ArrayList<IIconListAdapterItem>();
-        list.add(new MediaControlItem(mContext.getString(R.string.shortcut_media_play_pause), 
-                R.drawable.shortcut_media_play_pause, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE));
-        list.add(new MediaControlItem(mContext.getString(R.string.shortcut_media_previous),
-                R.drawable.shortcut_media_previous, KeyEvent.KEYCODE_MEDIA_PREVIOUS));
-        list.add(new MediaControlItem(mContext.getString(R.string.shortcut_media_next),
-                R.drawable.shortcut_media_next, KeyEvent.KEYCODE_MEDIA_NEXT));
+        list.add(new ShortcutItem(mContext, R.string.shortcut_media_play_pause, 
+                R.drawable.shortcut_media_play_pause, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_MEDIA_CONTROL,
+                                KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.shortcut_media_previous,
+                R.drawable.shortcut_media_previous, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_MEDIA_CONTROL,
+                                KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.shortcut_media_next,
+                R.drawable.shortcut_media_next, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_MEDIA_CONTROL,
+                                KeyEvent.KEYCODE_MEDIA_NEXT);
+                    }
+                }));
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext)
-            .setTitle(getText())
-            .setAdapter(new IconListAdapter(mContext, list), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    MediaControlItem item = (MediaControlItem) list.get(which);
-
-                    Intent launchIntent = new Intent(mContext, ShortcutActivity.class);
-                    launchIntent.setAction(ShortcutActivity.ACTION_LAUNCH_ACTION);
-                    launchIntent.putExtra(ShortcutActivity.EXTRA_ACTION, getAction());
-                    launchIntent.putExtra(ShortcutActivity.EXTRA_ACTION_TYPE, getActionType());
-                    launchIntent.putExtra(ModHwKeys.EXTRA_MEDIA_CONTROL, item.getAction());
-                    launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-                    Intent intent = new Intent();
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, item.getText());
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, item.getIconResource());
-
-                    MediaControlShortcut.this.onShortcutCreated(intent, listener);
-                    dialog.dismiss();
-                }
-            })
-            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        return list;
     }
 
     public static void launchAction(final Context context, Intent intent) {
@@ -111,45 +90,5 @@ public class MediaControlShortcut extends AShortcut {
         launchIntent.putExtra(ModHwKeys.EXTRA_MEDIA_CONTROL,
                 intent.getIntExtra(ModHwKeys.EXTRA_MEDIA_CONTROL, 0));
         context.sendBroadcast(launchIntent);
-    }
-
-    class MediaControlItem implements IIconListAdapterItem {
-        private String mLabel;
-        private int mIconResId;
-        private int mAction;
-
-        public MediaControlItem(String text, int iconResId, int action) {
-            mLabel = text;
-            mIconResId = iconResId;
-            mAction = action;
-        }
-
-        @Override
-        public String getText() {
-            return mLabel;
-        }
-
-        @Override
-        public String getSubText() {
-            return null;
-        }
-
-        @Override
-        public Drawable getIconLeft() {
-            return mResources.getDrawable(mIconResId);
-        }
-
-        @Override
-        public Drawable getIconRight() {
-            return null;
-        }
-
-        public ShortcutIconResource getIconResource() {
-            return ShortcutIconResource.fromContext(mContext, mIconResId);
-        }
-
-        public int getAction() {
-            return mAction;
-        }
     }
 }
