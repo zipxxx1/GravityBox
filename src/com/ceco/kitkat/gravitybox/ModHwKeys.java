ad4596f4eb7549ff81ea074161535f7454283ce5
C3C0 <ceco@ceco.sk.eu.org>
2016-04-13 16:17:24 +0200

    GB Actions: added explicit Auto brightness on/off shortcuts
    # Conflicts:
    #	src/com/ceco/kitkat/gravitybox/ModHwKeys.java
    #	src/com/ceco/kitkat/gravitybox/shortcuts/AutoBrightnessShortcut.java

--- src/com/ceco/kitkat/gravitybox/ModHwKeys.java
+++ src/com/ceco/kitkat/gravitybox/ModHwKeys.java
@@ -57,6 +57,7 @@ import android.widget.Toast;
 
 import com.ceco.kitkat.gravitybox.R;
 import com.ceco.kitkat.gravitybox.ledcontrol.QuietHoursActivity;
+import com.ceco.kitkat.gravitybox.shortcuts.AShortcut;
 import com.ceco.kitkat.gravitybox.shortcuts.RingerModeShortcut;
 import com.ceco.kitkat.gravitybox.shortcuts.ShortcutActivity;
 
@@ -387,7 +388,7 @@ public class ModHwKeys {
                     launchCustomApp(mHeadsetUri[state]);
                 }
             } else if (action.equals(ACTION_TOGGLE_AUTO_BRIGHTNESS)) {
-                toggleAutoBrightness();
+                changeAutoBrightnessState(intent);
             }
         }
     };
@@ -1788,15 +1789,24 @@ public class ModHwKeys {
         }
     }
 
-    private static void toggleAutoBrightness() {
+    private static void changeAutoBrightnessState(Intent intent) {
         try {
-            int brightnessMode = Settings.System.getInt(mContext.getContentResolver(),
+            int mode;
+            if (intent.hasExtra(AShortcut.EXTRA_ENABLE)) {
+                mode = intent.getBooleanExtra(AShortcut.EXTRA_ENABLE, false) ?
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC :
+                            Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+            } else {
+                int currentMmode = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.SCREEN_BRIGHTNESS_MODE, 0);
+                mode = currentMmode == Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL ?
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC :
+                            Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+            }
             Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS_MODE,
-                    brightnessMode == Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL ?
-                            Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC :
-                                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+                    Settings.System.SCREEN_BRIGHTNESS_MODE, mode);
+            Utils.postToast(mContext, mode == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC ?
+                    R.string.autobrightness_on : R.string.autobrightness_off);
         } catch (Throwable t) {
             XposedBridge.log(t);
         }
