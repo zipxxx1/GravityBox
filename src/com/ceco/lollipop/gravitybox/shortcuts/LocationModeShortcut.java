354527ecb4e361fa6417caa38ed79a0f1542f070
C3C0 <ceco@ceco.sk.eu.org>
2016-04-13 11:20:10 +0200

    Actions: reworked for abstract handling of multi-item shortcuts - Introduce abstract AMultiShortcut which implements common functionality for all actions having multiple items - Introduce ShortcutItem which is a common shortcut item that has label, icon and allows specifying ExtraDelegate which is responsible for adding action-specific extras to launching intent - Changed all existing multi-item shortcuts to be derived from AMultiShortcut # Conflicts: #	src/com/ceco/lollipop/gravitybox/shortcuts/LocationModeShortcut.java #	src/com/ceco/lollipop/gravitybox/shortcuts/MediaControlShortcut.java #	src/com/ceco/lollipop/gravitybox/shortcuts/NetworkModeShortcut.java #	src/com/ceco/lollipop/gravitybox/shortcuts/QuietHoursShortcut.java #	src/com/ceco/lollipop/gravitybox/shortcuts/RingerModeShortcut.java

--- src/com/ceco/lollipop/gravitybox/shortcuts/LocationModeShortcut.java
+++ src/com/ceco/lollipop/gravitybox/shortcuts/LocationModeShortcut.java
@@ -21,17 +21,13 @@ import java.util.List;
 import com.ceco.lollipop.gravitybox.ConnectivityServiceWrapper;
 import com.ceco.lollipop.gravitybox.R;
 import com.ceco.lollipop.gravitybox.adapters.IIconListAdapterItem;
-import com.ceco.lollipop.gravitybox.adapters.IconListAdapter;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.Intent.ShortcutIconResource;
 import android.graphics.drawable.Drawable;
 import android.provider.Settings;
 
-public class LocationModeShortcut extends AShortcut {
+public class LocationModeShortcut extends AMultiShortcut {
     protected static final String ACTION =  ConnectivityServiceWrapper.ACTION_SET_LOCATION_MODE;
 
     public LocationModeShortcut(Context context) {
@@ -45,7 +41,7 @@ public class LocationModeShortcut extends AShortcut {
 
     @Override
     public Drawable getIconLeft() {
-        return mResources.getDrawable(R.drawable.shortcut_gps_high);
+        return mResources.getDrawable(R.drawable.shortcut_gps_high, null);
     }
 
     @Override
@@ -59,53 +55,41 @@ public class LocationModeShortcut extends AShortcut {
     }
 
     @Override
-    protected ShortcutIconResource getIconResource() {
-        return null;
-    }
-
-    @Override
-    protected void createShortcut(final CreateShortcutListener listener) {
+    public List<IIconListAdapterItem> getShortcutList() {
         final List<IIconListAdapterItem> list = new ArrayList<IIconListAdapterItem>();
-        list.add(new LocationModeItem(mContext.getString(R.string.location_mode_high_accuracy), 
-                R.drawable.shortcut_gps_high, Settings.Secure.LOCATION_MODE_HIGH_ACCURACY));
-        list.add(new LocationModeItem(mContext.getString(R.string.location_mode_battery_saving),
-                R.drawable.shortcut_gps_saving, Settings.Secure.LOCATION_MODE_BATTERY_SAVING));
-        list.add(new LocationModeItem(mContext.getString(R.string.location_mode_device_only),
-                R.drawable.shortcut_gps_sensors, Settings.Secure.LOCATION_MODE_SENSORS_ONLY));
-        list.add(new LocationModeItem(mContext.getString(R.string.location_mode_off),
-                R.drawable.shortcut_gps_off, Settings.Secure.LOCATION_MODE_OFF));
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext)
-            .setTitle(getText())
-            .setAdapter(new IconListAdapter(mContext, list), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    LocationModeItem item = (LocationModeItem) list.get(which);
-
-                    Intent launchIntent = new Intent(mContext, LaunchActivity.class);
-                    launchIntent.setAction(ShortcutActivity.ACTION_LAUNCH_ACTION);
-                    launchIntent.putExtra(ShortcutActivity.EXTRA_ACTION, getAction());
-                    launchIntent.putExtra(ShortcutActivity.EXTRA_ACTION_TYPE, getActionType());
-                    launchIntent.putExtra(ConnectivityServiceWrapper.EXTRA_LOCATION_MODE, item.getLocationMode());
-                    launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-                    Intent intent = new Intent();
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, item.getText());
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, item.getIconResource());
-
-                    LocationModeShortcut.this.onShortcutCreated(intent, listener);
-                    dialog.dismiss();
-                }
-            })
-            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        list.add(new ShortcutItem(mContext, R.string.location_mode_high_accuracy, 
+                    R.drawable.shortcut_gps_high, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ConnectivityServiceWrapper.EXTRA_LOCATION_MODE,
+                                Settings.Secure.LOCATION_MODE_HIGH_ACCURACY);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext,R.string.location_mode_battery_saving,
+                R.drawable.shortcut_gps_saving, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ConnectivityServiceWrapper.EXTRA_LOCATION_MODE,
+                                Settings.Secure.LOCATION_MODE_BATTERY_SAVING);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.location_mode_device_only,
+                R.drawable.shortcut_gps_sensors, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ConnectivityServiceWrapper.EXTRA_LOCATION_MODE,
+                                Settings.Secure.LOCATION_MODE_SENSORS_ONLY);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.location_mode_off,
+                R.drawable.shortcut_gps_off, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ConnectivityServiceWrapper.EXTRA_LOCATION_MODE,
+                                Settings.Secure.LOCATION_MODE_OFF);
+                    }
+                }));
+        return list;
     }
 
     public static void launchAction(final Context context, Intent intent) {
@@ -114,45 +98,5 @@ public class LocationModeShortcut extends AShortcut {
                 intent.getIntExtra(ConnectivityServiceWrapper.EXTRA_LOCATION_MODE,
                         Settings.Secure.LOCATION_MODE_BATTERY_SAVING));
         context.sendBroadcast(launchIntent);
-    }
-
-    class LocationModeItem implements IIconListAdapterItem {
-        private String mLabel;
-        private int mIconResId;
-        private int mLocationMode;
-
-        public LocationModeItem(String text, int iconResId, int locationMode) {
-            mLabel = text;
-            mIconResId = iconResId;
-            mLocationMode = locationMode;
-        }
-
-        @Override
-        public String getText() {
-            return mLabel;
-        }
-
-        @Override
-        public String getSubText() {
-            return null;
-        }
-
-        @Override
-        public Drawable getIconLeft() {
-            return mResources.getDrawable(mIconResId);
-        }
-
-        @Override
-        public Drawable getIconRight() {
-            return null;
-        }
-
-        public ShortcutIconResource getIconResource() {
-            return ShortcutIconResource.fromContext(mContext, mIconResId);
-        }
-
-        public int getLocationMode() {
-            return mLocationMode;
-        }
     }
 }
--- src/com/ceco/lollipop/gravitybox/shortcuts/RingerModeShortcut.java
+++ src/com/ceco/lollipop/gravitybox/shortcuts/RingerModeShortcut.java
@@ -21,16 +21,12 @@ import java.util.List;
 import com.ceco.lollipop.gravitybox.ModHwKeys;
 import com.ceco.lollipop.gravitybox.R;
 import com.ceco.lollipop.gravitybox.adapters.IIconListAdapterItem;
-import com.ceco.lollipop.gravitybox.adapters.IconListAdapter;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.Intent.ShortcutIconResource;
 import android.graphics.drawable.Drawable;
 
-public class RingerModeShortcut extends AShortcut {
+public class RingerModeShortcut extends AMultiShortcut {
     protected static final String ACTION =  ModHwKeys.ACTION_SET_RINGER_MODE;
 
     public static final int MODE_RING = 0;
@@ -49,7 +45,7 @@ public class RingerModeShortcut extends AShortcut {
 
     @Override
     public Drawable getIconLeft() {
-        return mResources.getDrawable(R.drawable.shortcut_ringer_ring_vibrate);
+        return mResources.getDrawable(R.drawable.shortcut_ringer_ring_vibrate, null);
     }
 
     @Override
@@ -63,53 +59,38 @@ public class RingerModeShortcut extends AShortcut {
     }
 
     @Override
-    protected ShortcutIconResource getIconResource() {
-        return null;
-    }
-
-    @Override
-    protected void createShortcut(final CreateShortcutListener listener) {
+    protected List<IIconListAdapterItem> getShortcutList() {
         final List<IIconListAdapterItem> list = new ArrayList<IIconListAdapterItem>();
-        list.add(new RingerModeItem(mContext.getString(R.string.ringer_mode_sound), 
-                R.drawable.shortcut_ringer_ring, MODE_RING));
-        list.add(new RingerModeItem(mContext.getString(R.string.ringer_mode_sound_vibrate),
-                R.drawable.shortcut_ringer_ring_vibrate, MODE_RING_VIBRATE));
-        list.add(new RingerModeItem(mContext.getString(R.string.ringer_mode_silent),
-                R.drawable.shortcut_ringer_silent, MODE_SILENT));
-        list.add(new RingerModeItem(mContext.getString(R.string.ringer_mode_vibrate),
-                R.drawable.shortcut_ringer_vibrate, MODE_VIBRATE));
+        list.add(new ShortcutItem(mContext, R.string.ringer_mode_sound, 
+                R.drawable.shortcut_ringer_ring, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_RINGER_MODE, MODE_RING);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.ringer_mode_sound_vibrate,
+                R.drawable.shortcut_ringer_ring_vibrate, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_RINGER_MODE, MODE_RING_VIBRATE);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.ringer_mode_none,
+                R.drawable.shortcut_ringer_silent, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_RINGER_MODE, MODE_SILENT);
+                    }
+                }));
+        list.add(new ShortcutItem(mContext, R.string.ringer_mode_vibrate,
+                R.drawable.shortcut_ringer_vibrate, new ExtraDelegate() {
+                    @Override
+                    public void addExtraTo(Intent intent) {
+                        intent.putExtra(ModHwKeys.EXTRA_RINGER_MODE, MODE_VIBRATE);
+                    }
+                }));
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext)
-            .setTitle(getText())
-            .setAdapter(new IconListAdapter(mContext, list), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    RingerModeItem item = (RingerModeItem) list.get(which);
-
-                    Intent launchIntent = new Intent(mContext, LaunchActivity.class);
-                    launchIntent.setAction(ShortcutActivity.ACTION_LAUNCH_ACTION);
-                    launchIntent.putExtra(ShortcutActivity.EXTRA_ACTION, getAction());
-                    launchIntent.putExtra(ShortcutActivity.EXTRA_ACTION_TYPE, getActionType());
-                    launchIntent.putExtra(ModHwKeys.EXTRA_RINGER_MODE, item.getRingerMode());
-                    launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-                    Intent intent = new Intent();
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, item.getText());
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, item.getIconResource());
-
-                    RingerModeShortcut.this.onShortcutCreated(intent, listener);
-                    dialog.dismiss();
-                }
-            })
-            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        return list;
     }
 
     public static void launchAction(final Context context, Intent intent) {
@@ -118,45 +99,5 @@ public class RingerModeShortcut extends AShortcut {
                 intent.getIntExtra(ModHwKeys.EXTRA_RINGER_MODE,
                         MODE_RING_VIBRATE));
         context.sendBroadcast(launchIntent);
-    }
-
-    class RingerModeItem implements IIconListAdapterItem {
-        private String mLabel;
-        private int mIconResId;
-        private int mRingerMode;
-
-        public RingerModeItem(String text, int iconResId, int ringerMode) {
-            mLabel = text;
-            mIconResId = iconResId;
-            mRingerMode = ringerMode;
-        }
-
-        @Override
-        public String getText() {
-            return mLabel;
-        }
-
-        @Override
-        public String getSubText() {
-            return null;
-        }
-
-        @Override
-        public Drawable getIconLeft() {
-            return mResources.getDrawable(mIconResId);
-        }
-
-        @Override
-        public Drawable getIconRight() {
-            return null;
-        }
-
-        public ShortcutIconResource getIconResource() {
-            return ShortcutIconResource.fromContext(mContext, mIconResId);
-        }
-
-        public int getRingerMode() {
-            return mRingerMode;
-        }
     }
 }
