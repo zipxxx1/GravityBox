f732070a7dc2a43a14967015bc8ec54b7430697b
C3C0 <ceco@ceco.sk.eu.org>
2016-05-06 09:42:29 +0200

    KeyActions: explicitly show toast for auto rotation action Brings back previous behavior people got used to. When control over toast is needed it is still possible to use "Custom action" option, instead of direct Auto rotation key action.

--- src/com/ceco/lollipop/gravitybox/ModHwKeys.java
+++ src/com/ceco/lollipop/gravitybox/ModHwKeys.java
@@ -1513,7 +1513,9 @@ public class ModHwKeys {
     }
 
     private static void toggleAutoRotation() {
-        changeAutoRotationState(new Intent());
+        Intent intent = new Intent();
+        intent.putExtra(AShortcut.EXTRA_SHOW_TOAST, true);
+        changeAutoRotationState(intent);
     }
 
     private static void changeAutoRotationState(Intent intent) {
