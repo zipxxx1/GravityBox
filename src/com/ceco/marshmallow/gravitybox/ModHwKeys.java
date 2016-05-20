10b7d4f0e28bb04a0bea590cade97ccb35bb5366
C3C0 <ceco@ceco.sk.eu.org>
2016-05-20 11:51:56 +0200

    KeyActions: added option to allow volume rocker wake while music active

--- src/com/ceco/marshmallow/gravitybox/ModHwKeys.java
+++ src/com/ceco/marshmallow/gravitybox/ModHwKeys.java
@@ -141,6 +141,7 @@ public class ModHwKeys {
     private static int mDoubletapSpeed = GravityBoxSettings.HWKEY_DOUBLETAP_SPEED_DEFAULT;
     private static int mKillDelay = GravityBoxSettings.HWKEY_KILL_DELAY_DEFAULT;
     private static String mVolumeRockerWake = "default";
+    private static boolean mVolumeRockerWakeAllowMusic;
     private static boolean mHwKeysEnabled = true;
     private static XSharedPreferences mPrefs;
     private static int mPieMode;
@@ -300,8 +301,15 @@ public class ModHwKeys {
                 mKillDelay = value;
                 if (DEBUG) log("Kill delay set to: " + value);
             } else if (action.equals(GravityBoxSettings.ACTION_PREF_VOLUME_ROCKER_WAKE_CHANGED)) {
-                mVolumeRockerWake = intent.getStringExtra(GravityBoxSettings.EXTRA_VOLUME_ROCKER_WAKE);
-                if (DEBUG) log("mVolumeRockerWake set to: " + mVolumeRockerWake);
+                if (intent.hasExtra(GravityBoxSettings.EXTRA_VOLUME_ROCKER_WAKE)) {
+                    mVolumeRockerWake = intent.getStringExtra(GravityBoxSettings.EXTRA_VOLUME_ROCKER_WAKE);
+                    if (DEBUG) log("mVolumeRockerWake set to: " + mVolumeRockerWake);
+                }
+                if (intent.hasExtra(GravityBoxSettings.EXTRA_VOLUME_ROCKER_WAKE_ALLOW_MUSIC)) {
+                    mVolumeRockerWakeAllowMusic = intent.getBooleanExtra(
+                            GravityBoxSettings.EXTRA_VOLUME_ROCKER_WAKE_ALLOW_MUSIC, false);
+                    if (DEBUG) log("mVolumeRockerWakeAllowMusic set to: " + mVolumeRockerWakeAllowMusic);
+                }
             } else if (action.equals(GravityBoxSettings.ACTION_PREF_HWKEY_LOCKSCREEN_TORCH_CHANGED)) {
                 if (intent.hasExtra(GravityBoxSettings.EXTRA_HWKEY_TORCH)) {
                     mLockscreenTorch = intent.getIntExtra(GravityBoxSettings.EXTRA_HWKEY_TORCH,
@@ -495,6 +503,8 @@ public class ModHwKeys {
             mHomeDoubletapDisabled = prefs.getBoolean(
                     GravityBoxSettings.PREF_KEY_HWKEY_HOME_DOUBLETAP_DISABLE, false);
             mVolumeRockerWake = prefs.getString(GravityBoxSettings.PREF_KEY_VOLUME_ROCKER_WAKE, "default");
+            mVolumeRockerWakeAllowMusic = prefs.getBoolean(
+                    GravityBoxSettings.PREF_KEY_VOLUME_ROCKER_WAKE_ALLOW_MUSIC, false);
             mHwKeysEnabled = !prefs.getBoolean(GravityBoxSettings.PREF_KEY_HWKEYS_DISABLE, false);
 
             mPieMode = ModPieControls.PIE_DISABLED;
@@ -546,10 +556,10 @@ public class ModHwKeys {
 
                     if (!mVolumeRockerWake.equals("default") && 
                             (keyCode == KeyEvent.KEYCODE_VOLUME_UP ||
-                                    keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) &&
-                            !getAudioManager().isMusicActive()) {
+                                    keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
                         int policyFlags = (Integer) param.args[1];
-                        if (mVolumeRockerWake.equals("enabled")) {
+                        if (mVolumeRockerWake.equals("enabled") &&
+                            (!getAudioManager().isMusicActive() || mVolumeRockerWakeAllowMusic)) {
                             policyFlags |= FLAG_WAKE;
                             policyFlags |= FLAG_WAKE_DROPPED;
                         } else if (mVolumeRockerWake.equals("disabled")) {
@@ -832,9 +842,10 @@ public class ModHwKeys {
                     int keyCode = (Integer) param.args[0];
                     if (!mVolumeRockerWake.equals("default") && 
                             (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN ||
-                             keyCode == KeyEvent.KEYCODE_VOLUME_UP) &&
-                            !getAudioManager().isMusicActive()) {
-                        param.setResult(mVolumeRockerWake.equals("enabled") ? true : false);
+                             keyCode == KeyEvent.KEYCODE_VOLUME_UP)) {
+                        param.setResult(mVolumeRockerWake.equals("enabled") ? 
+                                (!getAudioManager().isMusicActive() || mVolumeRockerWakeAllowMusic) : 
+                                    false);
                     }
                 }
             });
