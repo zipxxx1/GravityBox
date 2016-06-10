f7275480a4e88e25358f8cff3edba9735ea5333f
C3C0 <ceco@ceco.sk.eu.org>
2016-06-10 09:33:07 +0200

    Audio: improved volume key swap feature
    Hook the last method in chain to make sure all different adjust volume
    calls are handled by hook
    # Conflicts:
    #	src/com/ceco/lollipop/gravitybox/ModAudio.java

--- src/com/ceco/kitkat/gravitybox/ModAudio.java
+++ src/com/ceco/kitkat/gravitybox/ModAudio.java
@@ -93,18 +93,17 @@ public class ModAudio {
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {    
                     Context context = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
                     if (context != null) {
-                        mHandleChangeVolume = new HandleChangeVolume(context);
-                        XposedHelpers.findAndHookMethod(classAudioService, "adjustMasterVolume", 
-                                int.class, int.class, String.class, mHandleChangeVolume);
-                        XposedHelpers.findAndHookMethod(classAudioService, "adjustSuggestedStreamVolume", 
-                                int.class, int.class, int.class, String.class, mHandleChangeVolume);
-
                         IntentFilter intentFilter = new IntentFilter();
                         intentFilter.addAction(GravityBoxSettings.ACTION_PREF_SAFE_MEDIA_VOLUME_CHANGED);
                         intentFilter.addAction(GravityBoxSettings.ACTION_PREF_VOL_FORCE_MUSIC_CONTROL_CHANGED);
                         intentFilter.addAction(GravityBoxSettings.ACTION_PREF_VOL_SWAP_KEYS_CHANGED);
                         context.registerReceiver(mBroadcastReceiver, intentFilter);
                         if (DEBUG) log("AudioService constructed. Broadcast receiver registered");
+
+                        mHandleChangeVolume = new HandleChangeVolume(context);
+                        XposedHelpers.findAndHookMethod(classAudioService, "adjustStreamVolume", 
+                                int.class, int.class, int.class, String.class,
+                                mHandleChangeVolume);
                     }
                     if (prefs.getBoolean(GravityBoxSettings.PREF_KEY_MUSIC_VOLUME_STEPS, false)) {
                         XposedHelpers.setIntField(param.thisObject, "mSafeMediaVolumeIndex", 150);
@@ -215,11 +214,11 @@ public class ModAudio {
         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
             if (mSwapVolumeKeys) {
                 try {
-                    if ((Integer) param.args[0] != 0) {
-                        if (DEBUG) log("Original direction = " + param.args[0]);
+                    if ((Integer) param.args[1] != 0) {
+                        if (DEBUG) log("Original direction = " + param.args[1]);
                         int orientation = getDirectionFromOrientation();
-                        param.args[0] = orientation * (Integer) param.args[0];
-                        if (DEBUG) log("Modified direction = " + param.args[0]);
+                        param.args[1] = orientation * (Integer) param.args[1];
+                        if (DEBUG) log("Modified direction = " + param.args[1]);
                     }
                 } catch (Throwable t) {
                     XposedBridge.log(t);
