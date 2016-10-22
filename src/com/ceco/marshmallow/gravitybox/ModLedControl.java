zipxxx1/GravityBox

commit a75a9692f95d98f9990e790159c75b73cfbf6845

UNC: added option for ignoring active screen for updated notifications
When enabled, active screen won't be performed for existing
notifications that got updated. Only for brand new notifications.

src/com/ceco/marshmallow/gravitybox/ModLedControl.java
@@ -221,6 +221,7 @@ public class ModLedControl {
                 Notification n = (Notification) param.args[6];
                 if (n.extras.containsKey("gbIgnoreNotification")) return;
 
+                Notification oldN = getOldNotification(param.args[0], param.args[4], param.args[5], param.args[8]);
                 final String pkgName = (String) param.args[0];
 
                 LedSettings ls = LedSettings.deserialize(mPrefs.getStringSet(pkgName, null));
@@ -233,46 +234,30 @@ public class ModLedControl {
                 }
                 if (DEBUG) log(pkgName + ": " + ls.toString());
 
+                boolean isOngoing = ((n.flags & Notification.FLAG_ONGOING_EVENT) != 0 || 
+                        (n.flags & Notification.FLAG_FOREGROUND_SERVICE) != 0);
+                // additional check if old notification had a foreground service flag set since it seems not to be propagated
+                // for updated notifications (until Notification gets processed by WorkerHandler which is too late for us)
+                if (!isOngoing && oldN != null) {
+                    isOngoing = (oldN.flags & Notification.FLAG_FOREGROUND_SERVICE) != 0;
+                    if (DEBUG) log("Old notification foreground service check: isOngoing=" + isOngoing);
+                }
+
                 final boolean qhActive = mQuietHours.quietHoursActive(ls, n, mUserPresent);
                 final boolean qhActiveIncludingLed = qhActive && mQuietHours.muteLED;
                 final boolean qhActiveIncludingVibe = qhActive && mQuietHours.muteVibe;
                 final boolean qhActiveIncludingActiveScreen = qhActive &&
                         !mPrefs.getBoolean(LedSettings.PREF_KEY_ACTIVE_SCREEN_IGNORE_QUIET_HOURS, false);
 
+                if (isOngoing && !ls.getOngoing() && !qhActive) {
+                    if (DEBUG) log("Ongoing led control disabled. Ignoring.");
+                    return;
+                }
+
                 if (ls.getEnabled()) {
                     n.extras.putBoolean(NOTIF_EXTRA_PROGRESS_TRACKING, ls.getProgressTracking());
                     n.extras.putString(NOTIF_EXTRA_VISIBILITY_LS, ls.getVisibilityLs().toString());
                     n.extras.putBoolean(NOTIF_EXTRA_HIDE_PERSISTENT, ls.getHidePersistent());
-                }
-
-                boolean isOngoing = ((n.flags & Notification.FLAG_ONGOING_EVENT) != 0 || 
-                        (n.flags & Notification.FLAG_FOREGROUND_SERVICE) != 0);
-                // additional check if old notification had a foreground service flag set since it seems not to be propagated
-                // for updated notifications (until Notification gets processed by WorkerHandler which is too late for us)
-                if (!isOngoing) {
-                    try {
-                        ArrayList<?> notifList = (ArrayList<?>) XposedHelpers.getObjectField(param.thisObject, "mNotificationList");
-                        synchronized (notifList) {
-                            int index = (Integer) XposedHelpers.callMethod(param.thisObject, "indexOfNotificationLocked",
-                                    param.args[0], param.args[4], param.args[5], param.args[8]);
-                            if (index >= 0) {
-                                Object oldNotif = notifList.get(index);
-                                if (oldNotif != null) {
-                                    Notification oldN = (Notification) XposedHelpers.callMethod(oldNotif, "getNotification");
-                                    isOngoing = (oldN.flags & Notification.FLAG_FOREGROUND_SERVICE) != 0;
-                                    if (DEBUG) log("Old notification foreground service check: isOngoing=" + isOngoing);
-                                }
-                            }
-                        }
-                    } catch (Throwable t) { 
-                        /* yet another vendor messing with the internals... */
-                        if (DEBUG) XposedBridge.log(t);
-                    }
-                }
-
-                if (isOngoing && !ls.getOngoing() && !qhActive) {
-                    if (DEBUG) log("Ongoing led control disabled. Ignoring.");
-                    return;
                 }
 
                 // lights
@@ -355,6 +340,7 @@ public class ModLedControl {
                     }
                     // active screen mode
                     if (ls.getActiveScreenMode() != ActiveScreenMode.DISABLED && 
+                            !(ls.getActiveScreenIgnoreUpdate() && oldN != null) &&
                             n.priority > Notification.PRIORITY_MIN &&
                             ls.getVisibilityLs() != VisibilityLs.CLEARABLE &&
                             ls.getVisibilityLs() != VisibilityLs.ALL &&
@@ -376,6 +362,28 @@ public class ModLedControl {
             }
         }
     };
+
+    private static Notification getOldNotification(Object pkg, Object tag, Object id, Object userId) {
+        Notification oldNotif = null;
+        try {
+            ArrayList<?> notifList = (ArrayList<?>) XposedHelpers.getObjectField(
+                    mNotifManagerService, "mNotificationList");
+            synchronized (notifList) {
+                int index = (Integer) XposedHelpers.callMethod(
+                        mNotifManagerService, "indexOfNotificationLocked",
+                        pkg, tag, id, userId);
+                if (index >= 0) {
+                    Object oldNotifRecord = notifList.get(index);
+                    oldNotif = (Notification) XposedHelpers.callMethod(oldNotifRecord, "getNotification");
+                }
+            }
+        } catch (Throwable t) {
+            log("Error in getOldNotification: " + t.getMessage());
+            if (DEBUG) XposedBridge.log(t);
+        }
+        if (DEBUG) log("getOldNotification: is old notification: " + (oldNotif != null));
+        return oldNotif;
+    }
 
     private static XC_MethodHook applyZenModeHook = new XC_MethodHook() {
         @SuppressWarnings("deprecation")

res/values/strings.xml
@@ -2021,4 +2021,9 @@
     <string name="pref_qs_brightness_icon_summary">Shows icon indicating current brightness mode. 
         Toggles adaptive brightness on single-press, opens display settings on long-press.</string>
 
+    <!-- UNC: Active screen: Ignore updated notifications -->
+    <string name="pref_lc_active_screen_ignore_update_title">Ignore updated notifications</string>
+    <string name="pref_lc_active_screen_ignore_update_summary">Won\'t perform Active screen when 
+        existing notification gets updated</string>
+
 </resources>

res/xml/led_control_settings.xml
@@ -173,6 +173,13 @@
             android:defaultValue="DO_NOTHING"
             android:persistent="false" />
 
+        <CheckBoxPreference
+            android:key="pref_lc_active_screen_ignore_update"
+            android:title="@string/pref_lc_active_screen_ignore_update_title"
+            android:summary="@string/pref_lc_active_screen_ignore_update_summary"
+            android:defaultValue="false"
+            android:persistent="false" />
+
     </PreferenceCategory>
 
     <PreferenceCategory 

src/com/ceco/marshmallow/gravitybox/ledcontrol/LedSettings.java
@@ -70,6 +70,7 @@ public class LedSettings {
     private String mVibratePatternStr;
     private long[] mVibratePattern;
     private ActiveScreenMode mActiveScreenMode;
+    private boolean mActiveScreenIgnoreUpdate;
     private LedMode mLedMode;
     private boolean mQhIgnore;
     private String mQhIgnoreList;
@@ -143,6 +144,8 @@ public class LedSettings {
             } else if (data[0].equals("activeScreenMode")) {
                 if ("HEADS_UP".equals(data[1])) data[1] = "DO_NOTHING";
                 ls.setActiveScreenMode(ActiveScreenMode.valueOf(data[1]));
+            } else if (data[0].equals("activeScreenIgnoreUpdate")) {
+                ls.setActiveScreenIgnoreUpdate(Boolean.valueOf(data[1]));
             } else if (data[0].equals("ledMode")) {
                 ls.setLedMode(LedMode.valueOf(data[1]));
             } else if (data[0].equals("qhIgnore")) {
@@ -195,6 +198,7 @@ public class LedSettings {
         mVibratePattern = null;
         mVibrateReplace = true;
         mActiveScreenMode = ActiveScreenMode.DISABLED;
+        mActiveScreenIgnoreUpdate = false;
         mLedMode = LedMode.OVERRIDE;
         mQhIgnore = false;
         mQhIgnoreList = null;
@@ -368,6 +372,10 @@ public class LedSettings {
         mActiveScreenMode = mode;
     }
 
+    protected void setActiveScreenIgnoreUpdate(boolean ignore) {
+        mActiveScreenIgnoreUpdate = ignore;
+    }
+
     protected void setLedMode(LedMode ledMode) {
         mLedMode = ledMode;
     }
@@ -508,6 +516,10 @@ public class LedSettings {
         return mActiveScreenMode;
     }
 
+    public boolean getActiveScreenIgnoreUpdate() {
+        return mActiveScreenIgnoreUpdate;
+    }
+
     public LedMode getLedMode() {
         return mLedMode;
     }
@@ -576,6 +588,7 @@ public class LedSettings {
                 dataSet.add("vibratePattern:" + mVibratePatternStr);
             }
             dataSet.add("activeScreenMode:" + mActiveScreenMode);
+            dataSet.add("activeScreenIgnoreUpdate:" + mActiveScreenIgnoreUpdate);
             dataSet.add("ledMode:" + mLedMode);
             dataSet.add("qhIgnore:" + mQhIgnore);
             if (mQhIgnoreList != null) {

src/com/ceco/marshmallow/gravitybox/ledcontrol/LedSettingsActivity.java
@@ -204,6 +204,7 @@ public class LedSettingsActivity extends Activity implements OnClickListener {
         mLedSettings.setVibrateOverride(mPrefsFragment.getVibrateOverride());
         mLedSettings.setVibratePatternFromString(mPrefsFragment.getVibratePatternAsString());
         mLedSettings.setActiveScreenMode(mPrefsFragment.getActiveScreenMode());
+        mLedSettings.setActiveScreenIgnoreUpdate(mPrefsFragment.getActiveScreenIgnoreUpdate());
         mLedSettings.setLedMode(mPrefsFragment.getLedMode());
         mLedSettings.setQhIgnore(mPrefsFragment.getQhIgnore());
         mLedSettings.setQhIgnoreList(mPrefsFragment.getQhIgnoreList());

src/com/ceco/marshmallow/gravitybox/ledcontrol/LedSettingsFragment.java
@@ -65,6 +65,7 @@ public class LedSettingsFragment extends PreferenceFragment implements OnPrefere
     private static final String PREF_KEY_DEFAULT_SETTINGS = "pref_lc_default_settings";
     private static final String PREF_CAT_KEY_ACTIVE_SCREEN = "pref_cat_lc_active_screen";
     private static final String PREF_KEY_ACTIVE_SCREEN_MODE = "pref_lc_active_screen_mode";
+    private static final String PREF_KEY_ACTIVE_SCREEN_IGNORE_UPDATE = "pref_lc_active_screen_ignore_update";
     private static final String PREF_KEY_LED_MODE = "pref_lc_led_mode";
     private static final String PREF_CAT_KEY_QH = "pref_cat_lc_quiet_hours";
     private static final String PREF_KEY_QH_IGNORE = "pref_lc_qh_ignore";
@@ -100,6 +101,7 @@ public class LedSettingsFragment extends PreferenceFragment implements OnPrefere
     private SwitchPreference mDefaultSettingsPref;
     private PreferenceCategory mActiveScreenCat;
     private ListPreference mActiveScreenModePref;
+    private CheckBoxPreference mActiveScreenIgnoreUpdatePref;
     private ListPreference mLedModePref;
     private PreferenceCategory mQhCat;
     private CheckBoxPreference mQhIgnorePref;
@@ -139,6 +141,7 @@ public class LedSettingsFragment extends PreferenceFragment implements OnPrefere
         mActiveScreenCat = (PreferenceCategory) findPreference(PREF_CAT_KEY_ACTIVE_SCREEN);
         mActiveScreenModePref = (ListPreference) findPreference(PREF_KEY_ACTIVE_SCREEN_MODE);
         mActiveScreenModePref.setOnPreferenceChangeListener(this);
+        mActiveScreenIgnoreUpdatePref = (CheckBoxPreference) findPreference(PREF_KEY_ACTIVE_SCREEN_IGNORE_UPDATE);
         mLedModePref = (ListPreference) findPreference(PREF_KEY_LED_MODE);
         mLedModePref.setOnPreferenceChangeListener(this);
         mQhCat = (PreferenceCategory) findPreference(PREF_CAT_KEY_QH);
@@ -189,6 +192,7 @@ public class LedSettingsFragment extends PreferenceFragment implements OnPrefere
         } else {
             mActiveScreenModePref.setValue(ledSettings.getActiveScreenMode().toString());
             mActiveScreenModePref.setSummary(mActiveScreenModePref.getEntry());
+            mActiveScreenIgnoreUpdatePref.setChecked(ledSettings.getActiveScreenIgnoreUpdate());
         }
         mLedModePref.setValue(ledSettings.getLedMode().toString());
         updateLedModeDependentState();
@@ -301,6 +305,10 @@ public class LedSettingsFragment extends PreferenceFragment implements OnPrefere
 
     protected ActiveScreenMode getActiveScreenMode() {
         return ActiveScreenMode.valueOf(mActiveScreenModePref.getValue());
+    }
+
+    protected boolean getActiveScreenIgnoreUpdate() {
+        return mActiveScreenIgnoreUpdatePref.isChecked();
     }
 
     protected LedMode getLedMode() {

commit 38cd8619aa117381d44ec0b874962be256fff4cb

UNC: fixed bug affecting Lockscreen visibility option
introduced in
https://github.com/GravityBox/GravityBox/commit/a75a9692f95d98f9990e790159c75b73cfbf6845
Closes #1605

src/com/ceco/marshmallow/gravitybox/ModLedControl.java
@@ -251,6 +251,19 @@ public class ModLedControl {
                 }
                 if (DEBUG) log(pkgName + ": " + ls.toString());
 
+                final boolean qhActive = mQuietHours.quietHoursActive(ls, n, mUserPresent);
+                final boolean qhActiveIncludingLed = qhActive && mQuietHours.muteLED;
+                final boolean qhActiveIncludingVibe = qhActive && mQuietHours.muteVibe;
+                final boolean qhActiveIncludingActiveScreen = qhActive &&
+                        !mPrefs.getBoolean(LedSettings.PREF_KEY_ACTIVE_SCREEN_IGNORE_QUIET_HOURS, false);
+
+                if (ls.getEnabled()) {
+                    n.extras.putBoolean(NOTIF_EXTRA_PROGRESS_TRACKING, ls.getProgressTracking());
+                    n.extras.putString(NOTIF_EXTRA_VISIBILITY_LS, ls.getVisibilityLs().toString());
+                    n.extras.putBoolean(NOTIF_EXTRA_HIDE_PERSISTENT, ls.getHidePersistent());
+                }
+
+                // whether to ignore ongoing notification
                 boolean isOngoing = ((n.flags & Notification.FLAG_ONGOING_EVENT) != 0 || 
                         (n.flags & Notification.FLAG_FOREGROUND_SERVICE) != 0);
                 // additional check if old notification had a foreground service flag set since it seems not to be propagated
@@ -259,22 +272,9 @@ public class ModLedControl {
                     isOngoing = (oldN.flags & Notification.FLAG_FOREGROUND_SERVICE) != 0;
                     if (DEBUG) log("Old notification foreground service check: isOngoing=" + isOngoing);
                 }
-
-                final boolean qhActive = mQuietHours.quietHoursActive(ls, n, mUserPresent);
-                final boolean qhActiveIncludingLed = qhActive && mQuietHours.muteLED;
-                final boolean qhActiveIncludingVibe = qhActive && mQuietHours.muteVibe;
-                final boolean qhActiveIncludingActiveScreen = qhActive &&
-                        !mPrefs.getBoolean(LedSettings.PREF_KEY_ACTIVE_SCREEN_IGNORE_QUIET_HOURS, false);
-
                 if (isOngoing && !ls.getOngoing() && !qhActive) {
                     if (DEBUG) log("Ongoing led control disabled. Ignoring.");
                     return;
-                }
-
-                if (ls.getEnabled()) {
-                    n.extras.putBoolean(NOTIF_EXTRA_PROGRESS_TRACKING, ls.getProgressTracking());
-                    n.extras.putString(NOTIF_EXTRA_VISIBILITY_LS, ls.getVisibilityLs().toString());
-                    n.extras.putBoolean(NOTIF_EXTRA_HIDE_PERSISTENT, ls.getHidePersistent());
                 }
 
                 // lights