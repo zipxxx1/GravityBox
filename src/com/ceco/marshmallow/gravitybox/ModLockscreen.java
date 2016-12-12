b9bd7bb2e07a1ef8335a2cbfa0d30fad27c9f84e
C3C0 <ceco@ceco.sk.eu.org>
2016-12-12 10:08:12 +0100

    Lockscreen: fixed SystemUI crash when setting bottom left action

--- src/com/ceco/marshmallow/gravitybox/ModLockscreen.java
+++ src/com/ceco/marshmallow/gravitybox/ModLockscreen.java
@@ -766,7 +766,7 @@ public class ModLockscreen {
         if (type != BottomAction.CUSTOM || action == null || action.isEmpty()) {
             mLeftAction = null;
         } else if (SysUiManagers.AppLauncher != null &&
-                (mLeftAction == null || !mLeftAction.getValue().equals(action))) {
+                (mLeftAction == null || !action.equals(mLeftAction.getValue()))) {
             mLeftAction = SysUiManagers.AppLauncher.createAppInfo();
             mLeftAction.setSizeDp(32);
             mLeftAction.initAppInfo(action);
