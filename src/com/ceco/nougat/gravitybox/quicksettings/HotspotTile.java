88ccf9b2191320d76e6eb01874c6122a8836e8e4
C3C0 <ceco@ceco.sk.eu.org>
2017-08-06 18:12:50 +0200

    QS: HotSpotTile: removed obsolete long click hook

--- src/com/ceco/nougat/gravitybox/quicksettings/HotspotTile.java
+++ src/com/ceco/nougat/gravitybox/quicksettings/HotspotTile.java
@@ -14,15 +14,9 @@
  */
 package com.ceco.nougat.gravitybox.quicksettings;
 
-import com.ceco.nougat.gravitybox.Utils;
-
 import android.content.ComponentName;
 import android.content.Intent;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.XC_MethodHook.Unhook;
 
 public class HotspotTile extends AospTile {
     public static final String AOSP_KEY = "hotspot";
@@ -30,13 +24,9 @@ public class HotspotTile extends AospTile {
     private static final Intent TETHER_SETTINGS = new Intent().setComponent(new ComponentName(
             "com.android.settings", "com.android.settings.TetherSettings"));
 
-    private Unhook mLongClickHook;
-
     protected HotspotTile(Object host, String aospKey, Object tile, XSharedPreferences prefs,
             QsTileEventDistributor eventDistributor) throws Throwable {
         super(host, aospKey, tile, prefs, eventDistributor);
-
-        createHooks();
     }
 
     @Override
@@ -48,37 +38,5 @@ public class HotspotTile extends AospTile {
     public boolean handleLongClick() {
         startSettingsActivity(TETHER_SETTINGS);
         return true;
-    }
-
-    @Override
-    public void handleDestroy() {
-        super.handleDestroy();
-        destroyHooks();
-    }
-
-    private void createHooks() {
-        if (Utils.isOxygenOs35Rom())
-            return;
-
-        try {
-            mLongClickHook = XposedHelpers.findAndHookMethod(mTile.getClass().getName(), 
-                    mContext.getClassLoader(), "handleLongClick", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    if (handleLongClick()) {
-                        param.setResult(null);
-                    }
-                }
-            });
-        } catch (Throwable t) {
-            XposedBridge.log(t);
-        }
-    }
-
-    private void destroyHooks() {
-        if (mLongClickHook != null) {
-            mLongClickHook.unhook();
-            mLongClickHook = null;
-        }
     }
 }
