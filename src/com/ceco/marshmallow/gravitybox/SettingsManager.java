6b57f9fc284edcd9cf4af3057f73987d9f3820b5
C3C0 <ceco@ceco.sk.eu.org>
2017-10-21 17:43:37 +0200

    Settings: warn in case Storage permission is disabled
    # Conflicts:
    #	src/com/ceco/marshmallow/gravitybox/SettingsManager.java

--- src/com/ceco/marshmallow/gravitybox/SettingsManager.java
+++ src/com/ceco/marshmallow/gravitybox/SettingsManager.java
@@ -18,8 +18,11 @@ package com.ceco.marshmallow.gravitybox;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
+
+import android.Manifest.permission;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.widget.Toast;
@@ -47,6 +50,12 @@ public class SettingsManager {
     }
 
     public boolean backupSettings() {
+        if (mContext.checkSelfPermission(permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                mContext.checkSelfPermission(permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(mContext, R.string.permission_storage_denied, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
         File targetDir = new File(BACKUP_PATH);
         if (!(targetDir.exists() && targetDir.isDirectory())) {
             if (!targetDir.mkdirs()) {
@@ -163,6 +172,12 @@ public class SettingsManager {
     }
 
     public boolean restoreSettings() {
+        if (mContext.checkSelfPermission(permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                mContext.checkSelfPermission(permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(mContext, R.string.permission_storage_denied, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
         if (!isBackupAvailable()) {
             Toast.makeText(mContext, R.string.settings_restore_no_backup, Toast.LENGTH_SHORT).show();
             return false;
