12571fcdf643d3b5829c900a6cfb717b23c191b6
Shauli <Shauli@Shauli-PC>
2018-01-26 14:27:03 +0200

    Fixed merge from upstream

--- src/com/ceco/oreo/gravitybox/ModLockscreen.java
+++ src/com/ceco/oreo/gravitybox/ModLockscreen.java
@@ -650,7 +650,6 @@ public class ModLockscreen {
                         if (param.thisObject == param.args[0] &&
                                 (int)param.args[1] == View.VISIBLE &&
                                 (mLeftAction != null || mLeftActionDrawableOrig != null)) {
-
                             XposedHelpers.callMethod(param.thisObject, Utils.isSamsungRom() ?
                                     "updateCameraVisibility" : "updateLeftAffordanceIcon");
                         }
