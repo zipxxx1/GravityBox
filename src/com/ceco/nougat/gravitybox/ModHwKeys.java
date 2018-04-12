4d1d148b748e1560d024cc59e2bd516f066e7489
C3C0 <ceco@ceco.sk.eu.org>
2018-04-12 12:48:02 +0200

    Force stop package during kill

--- src/com/ceco/nougat/gravitybox/ModHwKeys.java
+++ src/com/ceco/nougat/gravitybox/ModHwKeys.java
@@ -1321,6 +1321,7 @@ public class ModHwKeys {
                                     !cn.getPackageName().startsWith(defaultHomePackage)) {
                                 if (DEBUG) log("Force stopping: " + cn.getPackageName());
                                 XposedHelpers.callMethod(am, "removeTask", apps.get(0).id);
+                                XposedHelpers.callMethod(am, "forceStopPackage", cn.getPackageName());
                                 targetKilled = cn.getPackageName();
                             }
                         }
