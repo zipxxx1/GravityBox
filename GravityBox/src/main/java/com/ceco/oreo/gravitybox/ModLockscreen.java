75d27ebcc75c7d8bac5f4fec1124d845da2b6ee0
C3C0 <ceco@ceco.sk.eu.org>
2018-10-29 11:28:50 +0100

    Lockscreen: make Background opacity feature work with default wallpaper
    Existing Background opacity feature now applies also to default wallpaper.
    Feature can be turned off by setting opacity to 0. This means default (stock) behavior applies.
    Translators need to update summary text.
    
    Closed #1886

--- GravityBox/src/main/java/com/ceco/oreo/gravitybox/ModLockscreen.java
+++ GravityBox/src/main/java/com/ceco/oreo/gravitybox/ModLockscreen.java
@@ -285,20 +285,6 @@ public class ModLockscreen {
                         backDrop.animate().alpha(1f);
                         if (DEBUG) log("updateMediaMetaData: showing custom background");
                     }
-
-                    // opacity
-                    if (hasArtwork || mCustomBg != null) {
-                        backDropBack.getDrawable().clearColorFilter();
-                        final int opacity = mPrefs.getInt(
-                                GravityBoxSettings.PREF_KEY_LOCKSCREEN_BACKGROUND_OPACITY, 100);
-                        if (opacity != 100) {
-                            final int alpha = (int) ((1 - opacity / 100f) * 255);
-                            final int overlayColor = Color.argb(alpha, 0, 0, 0);
-                            backDropBack.getDrawable().mutate()
-                                .setColorFilter(overlayColor, PorterDuff.Mode.SRC_OVER);
-                            if (DEBUG) log("updateMediaMetaData: opacity set");
-                        }
-                    }
                 }
             });
 
@@ -688,6 +674,24 @@ public class ModLockscreen {
                 } catch (Throwable t) {
                     GravityBox.log(TAG, t);
                 }
+            }
+
+            // Keyguard scrim alpha (Background opacity)
+            try {
+                XposedHelpers.findAndHookMethod(CLASS_SCRIM_CONTROLLER, classLoader,
+                "scheduleUpdate", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        final int opacity = mPrefs.getInt(
+                                GravityBoxSettings.PREF_KEY_LOCKSCREEN_BACKGROUND_OPACITY, 0);
+                        if (opacity != 0) {
+                            final float alpha = (100 - opacity) / 100f;
+                            XposedHelpers.setFloatField(param.thisObject, "mScrimBehindAlphaKeyguard", alpha);
+                        }
+                    }
+                });
+            } catch (Throwable t) {
+                GravityBox.log(TAG, t);
             }
         } catch (Throwable t) {
             GravityBox.log(TAG, t);
