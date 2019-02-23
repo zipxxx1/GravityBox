44fd5cf6dd8d74366735b835e7a1acd0050768d3
C3C0 <ceco@ceco.sk.eu.org>
2019-02-23 12:36:24 +0100

    LockscreenAppBar: visual improvements
    - do not clip children when doing zoom in/out animation during safe launch
    - animate visibility change

--- GravityBox/src/main/java/com/ceco/pie/gravitybox/LockscreenAppBar.java
+++ GravityBox/src/main/java/com/ceco/pie/gravitybox/LockscreenAppBar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Peter Gregus for GravityBox Project (C3C076@xda)
+ * Copyright (C) 2019 Peter Gregus for GravityBox Project (C3C076@xda)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -114,9 +114,13 @@ public class LockscreenAppBar implements KeyguardStateMonitor.Listener,
             }
         }
         mContainer.addView(mRootView);
+        mContainer.setClipChildren(false);
+        if (mContainer.getParent() instanceof ViewGroup) {
+            ((ViewGroup)mContainer.getParent()).setClipChildren(false);
+        }
         updateScale();
 
-        mAppSlots = new ArrayList<AppInfo>(7);
+        mAppSlots = new ArrayList<>(7);
         mAppSlots.add(new AppInfo(R.id.quickapp1));
         mAppSlots.add(new AppInfo(R.id.quickapp2));
         mAppSlots.add(new AppInfo(R.id.quickapp3));
@@ -187,7 +191,22 @@ public class LockscreenAppBar implements KeyguardStateMonitor.Listener,
             }
         }
         mIsActive = atLeastOneVisible;
-        mRootView.setVisibility(mIsActive && mIsInteractive ? View.VISIBLE : View.GONE);
+        animateVisibility(mIsActive && mIsInteractive);
+    }
+
+    private void animateVisibility(final boolean visible) {
+        if (!visible) {
+            mRootView.setVisibility(View.GONE);
+            mRootView.setScaleX(0f);
+            mRootView.setScaleY(0f);
+        } else {
+            mRootView.setVisibility(View.VISIBLE);
+            mRootView.animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setDuration(300)
+                    .start();
+        }
     }
 
     @Override
@@ -205,7 +224,7 @@ public class LockscreenAppBar implements KeyguardStateMonitor.Listener,
     @Override
     public void onScreenStateChanged(boolean interactive) {
         mIsInteractive = interactive;
-        mRootView.setVisibility(mIsActive && mIsInteractive ? View.VISIBLE : View.GONE);
+        animateVisibility(mIsActive && mIsInteractive);
     }
 
     @Override
