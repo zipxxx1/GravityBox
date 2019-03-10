a5bd69d36298315ca790169385b59351c314cf30
C3C0 <ceco@ceco.sk.eu.org>
2019-03-10 14:07:14 +0100

    KeyguardStateMonitor: hook different method to track state changes
    The previous one did not work with EdXposed.

--- GravityBox/src/main/java/com/ceco/oreo/gravitybox/managers/KeyguardStateMonitor.java
+++ GravityBox/src/main/java/com/ceco/oreo/gravitybox/managers/KeyguardStateMonitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Peter Gregus for GravityBox Project (C3C076@xda)
+ * Copyright (C) 2019 Peter Gregus for GravityBox Project (C3C076@xda)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -109,8 +109,7 @@ public class KeyguardStateMonitor implements BroadcastSubReceiver {
                 }
             });
 
-            XposedHelpers.findAndHookMethod(CLASS_KG_MONITOR_IMPL, cl,
-                    "notifyKeyguardChanged", new XC_MethodHook() {
+            XposedBridge.hookAllMethods(monitorClass, "notifyKeyguardState", new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(final MethodHookParam param) {
                     boolean showing = XposedHelpers.getBooleanField(param.thisObject, "mShowing");
